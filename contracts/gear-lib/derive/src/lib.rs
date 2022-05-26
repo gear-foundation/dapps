d531bf7641aaeaf497f11b440757fb396d97b030
Shamil <66209982+shamilsan@users.noreply.github.com>
2022-05-26 11:09:16 +0300

    Fix library (#2)

--- contracts/gear-lib/derive/src/lib.rs
+++ contracts/gear-lib/derive/src/lib.rs
@@ -3,10 +3,8 @@
 extern crate alloc;
 extern crate proc_macro;
 
-mod macros;
-
 #[allow(unused_imports)]
-use macros::*;
+use gear_lib_macros::*;
 
 declare_derive_storage_trait!(derive_mtk_state, StateKeeper, MTKStateKeeper);
 declare_impl_trait!(derive_mtk_token_state, MTKTokenState);
