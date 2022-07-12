184b4a00058bee5e2b3cec1db8222472b8f73118
Kirill Prosvirov <prosvirov.k@gmail.com>
2022-07-11 22:53:25 -0700

    Adding missing macros for FT (#7)
    * Adding macros
    
    * modify mint a bit

--- contracts/gear-lib-ng/derive/src/lib.rs
+++ contracts/gear-lib-ng/derive/src/lib.rs
@@ -11,3 +11,8 @@ declare_impl_trait!(derive_nft_metastate, NFTMetaState);
 declare_derive_storage_trait!(derive_mtk_state, StateKeeper, MTKStateKeeper);
 declare_impl_trait!(derive_mtk_token_state, MTKTokenState);
 declare_impl_trait!(derive_mtk_core, MTKCore);
+
+// FT
+declare_derive_storage_trait!(derive_ft_state, FTStateKeeper, FTStateField);
+declare_impl_trait!(derive_ft_core, FTCore);
+declare_impl_trait!(derive_ft_metastate, FTMetaState);
