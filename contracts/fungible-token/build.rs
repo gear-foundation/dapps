81967dd4aacfc6d6ddeac0f5b84f863079e22d3c
Ivan Azoyan <azoyan.89@mail.ru>
2023-01-29 21:30:37 +0300

    Add new meta (#29)
    * Add new meta
    
    * Use non stable gstd version
    
    * add lib.rs
    
    * Fix PR drawbacks
    
    * Fix metahash function

--- contracts/fungible-token/build.rs
+++ contracts/fungible-token/build.rs
@@ -1,3 +1,5 @@
+use ft_io::FungibleTokenMetadata;
+
 fn main() {
-    gear_wasm_builder::build();
+    gear_wasm_builder::build_with_metadata::<FungibleTokenMetadata>();
 }
--- contracts/fungible-token/io/src/lib.rs
+++ contracts/fungible-token/io/src/lib.rs
@@ -1,6 +1,18 @@
 #![no_std]
 
+use gmeta::{In, InOut, Metadata};
 use gstd::{prelude::*, ActorId};
+
+pub struct FungibleTokenMetadata;
+
+impl Metadata for FungibleTokenMetadata {
+    type Init = In<InitConfig>;
+    type Handle = InOut<FTAction, FTEvent>;
+    type Others = ();
+    type Reply = ();
+    type Signal = ();
+    type State = IoFungibleToken;
+}
 
 #[derive(Debug, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
@@ -46,4 +58,16 @@ pub enum FTEvent {
     },
     TotalSupply(u128),
     Balance(u128),
+}
+
+#[derive(Debug, Clone, Default, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct IoFungibleToken {
+    pub name: String,
+    pub symbol: String,
+    pub total_supply: u128,
+    pub balances: Vec<(ActorId, u128)>,
+    pub allowances: Vec<(ActorId, Vec<(ActorId, u128)>)>,
+    pub decimals: u8,
 }
