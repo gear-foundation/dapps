30c573847749a68973b221fd08ad60977a509b39
ereburg <gimmethenick@gmail.com>
2023-02-14 14:20:37 +0300

    upd: logo and icons

--- contracts/tequila-train/frontend/src/components/common/get-gas-balance/get-gas-balance.tsx
+++ contracts/tequila-train/frontend/src/components/common/get-gas-balance/get-gas-balance.tsx
@@ -7,12 +7,6 @@ export const GetGasBalance = () => {
     <div className="">
       <button className={clsx('btn group !p-2.5', buttonStyles.lightGreen)}>
         <Icon name="test-balance" width={20} height={20} />
-        <Icon
-          name="plus"
-          width={12}
-          height={12}
-          className="absolute bottom-2 right-1.5 bg-[#223428] group-hover:bg-[#285b3a] rounded-full transition-colors"
-        />
       </button>
     </div>
   );
--- contracts/tequila-train/frontend/src/components/layout/header/logo/logo.tsx
+++ contracts/tequila-train/frontend/src/components/layout/header/logo/logo.tsx
@@ -11,8 +11,9 @@ export const Logo = () => {
           <Icon name="logo-game" width={180} height={44} className="h-10" />
         </Link>
       ) : (
-        <span className="inline-flex">
-          <Icon name="logo-game" width={180} height={44} className="h-10" />
+        <span className="inline-flex items-end gap-3">
+          <Icon name="train" width={43} height={35} className="w-auto h-9" />
+          <Icon name="logo-game" width={140} height={21} className="w-auto h-5 mb-1" />
         </span>
       )}
     </>
