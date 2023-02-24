175e5bf734f2c140e96305df0250bdf7df8d3fd9
Ilia <56516965+bezrazli4n0@users.noreply.github.com>
2023-02-24 15:05:27 +0300

    fix: change gear crates branch to 'testnet' (#20)

--- contracts/ping/state/src/lib.rs
+++ contracts/ping/state/src/lib.rs
@@ -5,22 +5,22 @@ use gmeta::{metawasm, Metadata};
 use gstd::prelude::*;
 
 #[metawasm]
-pub trait Metawasm {
-    type State = <DemoPingMetadata as Metadata>::State;
+pub mod metafns {
+    pub type State = <DemoPingMetadata as Metadata>::State;
 
-    fn get_first_message(state: Self::State) -> String {
+    pub fn get_first_message(state: State) -> String {
         state.first().expect("Message log is empty!").to_string()
     }
 
-    fn get_last_message(state: Self::State) -> String {
+    pub fn get_last_message(state: State) -> String {
         state.last().expect("Message log is empty!").to_string()
     }
 
-    fn get_messages_len(state: Self::State) -> u64 {
+    pub fn get_messages_len(state: State) -> u64 {
         state.len() as u64
     }
 
-    fn get_message(index: u64, state: Self::State) -> String {
+    pub fn get_message(state: State, index: u64) -> String {
         state
             .get(index as usize)
             .expect("Invalid index!")
