d2045b67f4ed9b39322542432a8e2bd2bc44c76a
LouiseMedova <luisa.medova@gmail.com>
2023-03-04 20:25:12 +0300

    cargo update

--- contracts/tamagotchi/state/src/lib.rs
+++ contracts/tamagotchi/state/src/lib.rs
@@ -8,10 +8,10 @@ pub const BOREDOM_PER_BLOCK: u64 = 2;
 pub const ENERGY_PER_BLOCK: u64 = 2;
 
 #[metawasm]
-pub trait Metawasm {
-    type State = Tamagotchi;
+pub mod metafns {
+    pub type State = Tamagotchi;
 
-    fn current_state(state: Self::State) -> TmgCurrentState {
+    pub fn current_state(state: State) -> TmgCurrentState {
         let fed = state.fed.saturating_sub(HUNGER_PER_BLOCK * ((exec::block_timestamp() - state.fed_block)/1_000));
         let entertained = state.entertained.saturating_sub(BOREDOM_PER_BLOCK * ((exec::block_timestamp() - state.entertained_block)/1_000));
         let rested = state.rested.saturating_sub( ENERGY_PER_BLOCK * ((exec::block_timestamp() - state.rested_block)/1_000));
@@ -21,8 +21,8 @@ pub trait Metawasm {
 }
 
 #[derive(Encode, Decode, TypeInfo)]
-struct TmgCurrentState {
-    fed: u64,
-    entertained: u64,
-    rested: u64,
+pub struct TmgCurrentState {
+    pub fed: u64,
+    pub entertained: u64,
+    pub rested: u64,
 }
