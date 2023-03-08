cf2b55989be693f6f9dc2e0f902e0910cbb01fae
Fluid <90795031+fluiderson@users.noreply.github.com>
2023-03-08 20:22:05 +0300

    Update deps (#25)
    * Update
    
    * Fix CI

--- contracts/multisig-wallet/state/src/lib.rs
+++ contracts/multisig-wallet/state/src/lib.rs
@@ -22,58 +22,57 @@ fn common_confirmations_count(
 }
 
 #[metawasm]
-pub trait Metawasm {
-    type State = <ContractMetadata as Metadata>::State;
+pub mod metafns {
+    pub type State = <ContractMetadata as Metadata>::State;
 
-    // Returns number of confirmations of a transaction.
-    // `transaction_id` Transaction ID.
-    // Number of confirmations.
-    fn confirmations_count(transaction_id: TransactionId, state: Self::State) -> Option<u32> {
+    /// Returns number of confirmations of a transaction.
+    /// `transaction_id` Transaction ID.
+    /// Number of confirmations.
+    pub fn confirmations_count(state: State, transaction_id: TransactionId) -> Option<u32> {
         common_confirmations_count(&state, transaction_id)
     }
 
-    // Returns total number of transactions after filers are applied.
-    // `pending` Include pending transactions.
-    // `executed` Include executed transactions.
-    // Total number of transactions after filters are applied.
-    fn transactions_count(pending_executed: PendingExecuted, state: Self::State) -> u32 {
+    /// Returns total number of transactions after filers are applied.
+    /// `pending` Include pending transactions.
+    /// `executed` Include executed transactions.
+    /// Total number of transactions after filters are applied.
+    pub fn transactions_count(state: State, pending: bool, executed: bool) -> u32 {
         state
             .transactions
             .into_iter()
-            .filter(|(_, tx)| {
-                (pending_executed.0 && !tx.executed) || (pending_executed.1 && tx.executed)
-            })
+            .filter(|(_, tx)| (pending && !tx.executed) || (executed && tx.executed))
             .count() as _
     }
 
-    // Returns list of owners.
-    // List of owner addresses.
-    fn owners(state: Self::State) -> Vec<ActorId> {
+    /// Returns list of owners.
+    /// List of owner addresses.
+    pub fn owners(state: State) -> Vec<ActorId> {
         state.owners
     }
 
-    // Returns array with owner addresses, which confirmed transaction.
-    // `transaction_id` Transaction ID.
-    // Returns array of owner addresses.
-    fn confirmations(transaction_id: TransactionId, state: Self::State) -> Option<Vec<ActorId>> {
+    /// Returns array with owner addresses, which confirmed transaction.
+    /// `transaction_id` Transaction ID.
+    /// Returns array of owner addresses.
+    pub fn confirmations(state: State, transaction_id: TransactionId) -> Option<Vec<ActorId>> {
         state
             .confirmations
             .into_iter()
             .find_map(|(tx_id, confirmations)| (tx_id == transaction_id).then_some(confirmations))
     }
 
-    // Returns list of transaction IDs in defined range.
-    // `from` Index start position of transaction array.
-    // `to` Index end position of transaction array(not included).
-    // `pending` Include pending transactions.
-    // `executed` Include executed transactions.
-    // `Returns` array of transaction IDs.
-    fn transaction_ids(
-        from_to_pending_executed: FromToPendingExecuted,
-        state: Self::State,
+    /// Returns list of transaction IDs in defined range.
+    /// `from` Index start position of transaction array.
+    /// `to` Index end position of transaction array(not included).
+    /// `pending` Include pending transactions.
+    /// `executed` Include executed transactions.
+    /// `Returns` array of transaction IDs.
+    pub fn transaction_ids(
+        state: State,
+        from: u32,
+        to: u32,
+        pending: bool,
+        executed: bool,
     ) -> Vec<TransactionId> {
-        let (from, to, pending, executed) = from_to_pending_executed;
-
         state
             .transactions
             .into_iter()
@@ -84,9 +83,9 @@ pub trait Metawasm {
             .collect()
     }
 
-    // Returns the confirmation status of a transaction.
-    // `transaction_id` Transaction ID.
-    fn is_confirmed(transaction_id: TransactionId, state: Self::State) -> bool {
+    /// Returns the confirmation status of a transaction.
+    /// `transaction_id` Transaction ID.
+    pub fn is_confirmed(state: State, transaction_id: TransactionId) -> bool {
         let required = state.required;
 
         if let Some(count) = common_confirmations_count(&state, transaction_id) {
@@ -96,11 +95,11 @@ pub trait Metawasm {
         }
     }
 
-    // Returns the description of a transaction.
-    // `transaction_id` Transaction ID.
-    fn transaction_description(
+    /// Returns the description of a transaction.
+    /// `transaction_id` Transaction ID.
+    pub fn transaction_description(
+        state: State,
         transaction_id: TransactionId,
-        state: Self::State,
     ) -> Option<Option<String>> {
         state
             .transactions
@@ -108,6 +107,3 @@ pub trait Metawasm {
             .find_map(|(tx_id, tx)| (tx_id == transaction_id).then_some(tx.description))
     }
 }
-
-pub type PendingExecuted = (bool, bool);
-pub type FromToPendingExecuted = (u32, u32, bool, bool);
