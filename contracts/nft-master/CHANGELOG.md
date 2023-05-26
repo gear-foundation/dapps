665d6eb11aa7386f9bce48bf38d6672f3d47c836
b3zrazli4n0 <b3zrazli4n0@gmail.com>
2023-05-26 18:44:54 +0300

    feat: added contract impl, tests, ci, workspace split, pin gear crates

--- contracts/nft-master/CHANGELOG.md
+++ contracts/nft-master/CHANGELOG.md
@@ -6,9 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.1.0] - 2022-07-13
+## [0.1.0] - 2023-05-26
 ### Added
 - Initial release.
 
-[Unreleased]: https://github.com/gear-dapps/app/compare/0.1.0...HEAD
-[0.1.0]: https://github.com/gear-dapps/app/compare/a40d727...0.1.0
+[Unreleased]: https://github.com/gear-dapps/nft-master/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/gear-dapps/nft-master/compare/7822924...0.1.0
--- contracts/nft-master/state/src/lib.rs
+++ contracts/nft-master/state/src/lib.rs
@@ -1,24 +1,21 @@
 #![no_std]
 
-use app_io::*;
 use gmeta::{metawasm, Metadata};
 use gstd::{prelude::*, ActorId};
+use nft_master_io::*;
 
 #[cfg(feature = "binary-vendor")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 #[metawasm]
 pub mod metafns {
-    pub type State = <ContractMetadata as Metadata>::State;
+    pub type State = <NFTMasterMetadata as Metadata>::State;
 
-    pub fn pingers(state: State) -> Vec<ActorId> {
-        state.into_iter().map(|(pinger, _)| pinger).collect()
+    pub fn get_nfts(state: State) -> Vec<(ActorId, String)> {
+        state.nfts
     }
 
-    pub fn ping_count(state: State, actor: ActorId) -> u128 {
-        state
-            .into_iter()
-            .find_map(|(pinger, ping_count)| (pinger == actor).then_some(ping_count))
-            .unwrap_or_default()
+    pub fn get_operators(state: State) -> Vec<ActorId> {
+        state.operators
     }
 }
