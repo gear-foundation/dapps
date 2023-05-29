0422adbf2ea2f9814fd07797a12bc101e1c1a862
Nikita Yutanov <nikitayutanov@gmail.com>
2023-05-29 20:16:49 +0300

    Add welcome page

--- frontend/nft-master/src/pages/home/Home.tsx
+++ frontend/nft-master/src/pages/home/Home.tsx
@@ -1,5 +1,32 @@
+import { Button } from '@gear-js/ui';
+import { useState } from 'react';
+import { WalletModal } from 'features/wallet';
+import { useAccount } from '@gear-js/react-hooks';
+import styles from './Home.module.scss';
+
 function Home() {
-  return <div>Home page</div>;
+  const { account } = useAccount();
+
+  const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
+
+  const openWalletModal = () => setIsWalletModalOpen(true);
+  const closeWalletModal = () => setIsWalletModalOpen(false);
+
+  return account ? null : (
+    <div className={styles.welcome}>
+      <h2 className={styles.heading}>
+        Vara <span className={styles.nftText}>NFT</span>
+      </h2>
+
+      <p className={styles.text}>
+        A simple application that shows user&apos;s NFTs in different gear networks and contracts
+      </p>
+
+      <Button text="Connect Account" className={styles.button} onClick={openWalletModal} />
+
+      {isWalletModalOpen && <WalletModal onClose={closeWalletModal} />}
+    </div>
+  );
 }
 
 export { Home };
