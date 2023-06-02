54ce6ed5c41ccbdaae618182476df5a1f47ff08d
Nikita Yutanov <nikitayutanov@gmail.com>
2023-06-02 20:22:15 +0300

    Add env example

--- frontend/nft-master/src/consts.ts
+++ frontend/nft-master/src/consts.ts
@@ -3,7 +3,7 @@ import { HexString } from '@polkadot/util/types';
 const ADDRESS = {
   DETAULT_NODE: process.env.REACT_APP_NODE_ADDRESS as string,
   DEFAULT_NODES: process.env.REACT_APP_DEFAULT_NODES_URL as string,
-  DEFAULT_CONTRACT: process.env.REACT_APP_DEFAULT_CONTRACT_ADDRESS as HexString,
+  DEFAULT_CONTRACT: process.env.REACT_APP_DEFAULT_CONTRACT_ADDRESS as HexString | undefined,
   IPFS: process.env.REACT_APP_IPFS_ADDRESS as string,
   IPFS_GATEWAY: process.env.REACT_APP_IPFS_GATEWAY_ADDRESS as string,
 };
--- frontend/nft-master/src/features/contract-address/consts.ts
+++ frontend/nft-master/src/features/contract-address/consts.ts
@@ -2,8 +2,8 @@ import { HexString } from '@polkadot/util/types';
 import { atom } from 'jotai';
 import { ADDRESS, LOCAL_STORAGE } from 'consts';
 
-const CONTRACT_ADDRESS_ATOM = atom<HexString | undefined>(
-  (localStorage[LOCAL_STORAGE.CONTRACT_ADDRESS] as HexString) || ADDRESS.DEFAULT_CONTRACT,
+const CONTRACT_ADDRESS_ATOM = atom(
+  (localStorage[LOCAL_STORAGE.CONTRACT_ADDRESS] as HexString | null) || ADDRESS.DEFAULT_CONTRACT,
 );
 
 export { CONTRACT_ADDRESS_ATOM };
