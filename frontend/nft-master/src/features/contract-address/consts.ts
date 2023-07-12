88df945fe3d0e6c67de478772555e5fd6a06f7ed
Nikita Yutanov <nikitayutanov@gmail.com>
2023-07-12 19:24:47 +0300

    Temporary disable local storage master contract address

--- frontend/nft-master/src/features/contract-address/consts.ts
+++ frontend/nft-master/src/features/contract-address/consts.ts
@@ -2,6 +2,7 @@ import { atom } from 'jotai';
 import { ADDRESS } from 'consts';
 import { getLocalStorageMasterId, getSearchParamsMasterId } from './utils';
 
-const CONTRACT_ADDRESS_ATOM = atom(getSearchParamsMasterId() || getLocalStorageMasterId() || ADDRESS.DEFAULT_CONTRACT);
+// const CONTRACT_ADDRESS_ATOM = atom(getSearchParamsMasterId() || getLocalStorageMasterId() || ADDRESS.DEFAULT_CONTRACT);
+const CONTRACT_ADDRESS_ATOM = atom(getSearchParamsMasterId() || ADDRESS.DEFAULT_CONTRACT);
 
 export { CONTRACT_ADDRESS_ATOM };
