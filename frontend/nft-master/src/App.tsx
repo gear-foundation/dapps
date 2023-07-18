3366edb69e09b3d340752c32c2ff5479566ad5d1
Nikita Yutanov <nikitayutanov@gmail.com>
2023-07-18 15:59:57 +0300

    Remove dev leftovers

--- frontend/nft-master/src/App.tsx
+++ frontend/nft-master/src/App.tsx
@@ -1,25 +1,18 @@
-import { useApi, useAccount, useAlert } from '@gear-js/react-hooks';
+import { useApi, useAccount } from '@gear-js/react-hooks';
 import { Header, Footer, ApiLoader, Loader } from 'components';
 import { Routing } from 'pages';
 import { withProviders } from 'hocs';
 import { useNFTsState } from 'features/nfts';
 import { useContractAddressSetup } from 'features/contract-address';
 import 'App.scss';
-import { useEffect } from 'react';
 
 function Component() {
-  const alert = useAlert();
   const { isApiReady } = useApi();
   const { isAccountReady } = useAccount();
   const isAppReady = isApiReady && isAccountReady;
 
   useContractAddressSetup();
   const isNFTsStateReady = useNFTsState();
-
-  useEffect(() => {
-    alert.error('Test', { timeout: 1000000 });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
