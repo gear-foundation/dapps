25b00adbdf8ba735da47b3d9b07ce83262e6cb18
ereburg <gimmethenick@gmail.com>
2023-08-08 16:12:05 +0300

    upd envs

--- contracts/tamagotchi-battle/frontend-testnet/src/env.d.ts
+++ contracts/tamagotchi-battle/frontend-testnet/src/env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_FT_ADDRESS: string
   readonly VITE_AUTH_API_ADDRESS: string
   readonly VITE_TESTNET_WEBSITE_ADDRESS: string
-  readonly VITE_BATTLE_ADDRESS: string
 }
 
 interface ImportMeta {
