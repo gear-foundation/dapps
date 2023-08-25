204b68f166fd3639200bd49d348286bcd6b01c35
ereburg <gimmethenick@gmail.com>
2023-08-25 16:50:53 +0300

    add 404 page

--- frontend/nft-master/src/App.tsx
+++ frontend/nft-master/src/App.tsx
@@ -5,12 +5,14 @@ import { withProviders } from 'hocs';
 import { useNFTsState, useTestnetAutoLogin } from 'features/nfts';
 import { useSearchParamsSetup } from 'features/node-switch';
 import 'App.scss';
+import { useEffect, useRef } from 'react';
 import { useTestnetNFTSetup } from './features/nfts/hooks';
 
 function Component() {
   const { isApiReady } = useApi();
   const { isAccountReady } = useAccount();
   const isAppReady = isApiReady && isAccountReady;
+  const ref = useRef<null | number>(null);
 
   useSearchParamsSetup();
   useTestnetAutoLogin();
@@ -18,6 +20,17 @@ function Component() {
   const isNFTStateReady = useNFTsState();
   const isTestnetStateReady = useTestnetNFTSetup();
   const isEachStateReady = isNFTStateReady && isTestnetStateReady;
+
+  useEffect(() => {
+    if (!ref.current) ref.current = performance.now();
+    console.log({ isNFTStateReady, isTestnetStateReady });
+
+    if (isNFTStateReady && ref.current) {
+      const diff = Math.floor((performance.now() - ref.current) * 1000) / 1_000_000;
+      console.log(`${diff} seconds`);
+      ref.current = null;
+    }
+  }, [isNFTStateReady, isTestnetStateReady]);
 
   return (
     <>
