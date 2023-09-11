0c265b02f16486ebd2a0309d4c25fb0a8076e9b5
ereburg <gimmethenick@gmail.com>
2023-09-11 13:23:37 +0300

    add GTM analytics module; increase app version

--- frontend/nft-master/src/index.tsx
+++ frontend/nft-master/src/index.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'keen-slider/keen-slider.min.css';
+import TagManager from 'react-gtm-module';
 import { App } from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  TagManager.initialize({
+    gtmId: 'GTM-NPRC6D7P',
+  });
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
