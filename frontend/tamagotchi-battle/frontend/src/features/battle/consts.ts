fb1f6b2523a9afc4a02090649fbdee44d10b4a97
Yauheni <avegen.dr@gmail.com>
2023-10-05 17:57:58 +0300

    Updates consts

--- frontend/tamagotchi-battle/frontend/src/features/battle/consts.ts
+++ frontend/tamagotchi-battle/frontend/src/features/battle/consts.ts
@@ -1,3 +1,3 @@
-import { HexString } from '@polkadot/util/types'
+import { HexString } from '@polkadot/util/types';
 
-export const BATTLE_ADDRESS = process.env.REACT_APP_GAME_ADDRESS as HexString
+export const BATTLE_ADDRESS = '0x644c1e6f60cd2e7ddcb503ae4923fb5486dddaf9933733d7bc473fbaef67efc8';
