a8c505f2ecc65864eeb3933c20c3003a08b3b617
Nikita Yutanov <nikitayutanov@gmail.com>
2023-10-20 19:17:49 +0300

    Fix stats update

--- frontend/tamagotchi-academy/src/app/hooks/use-read-wasm-state.ts
+++ frontend/tamagotchi-academy/src/app/hooks/use-read-wasm-state.ts
@@ -1,5 +1,5 @@
-import { useEffect } from 'react'
-import { useReadWasmState, withoutCommas } from '@gear-js/react-hooks'
+import { useEffect, useState } from 'react'
+import { useAlert, useApi, withoutCommas } from '@gear-js/react-hooks'
 import { useLessons, useTamagotchi } from '@/app/context'
 import { useStateMetadata } from './use-metadata'
 import { sleep } from '@/app/utils'
@@ -7,11 +7,131 @@ import type { TamagotchiState } from '@/app/types/lessons'
 import state2 from '@/assets/meta/state2.meta.wasm?url'
 import { useLessonAssets } from '../utils/get-lesson-assets'
 
+import {
+  MessagesDispatched,
+  ProgramMetadata,
+  getStateMetadata,
+} from '@gear-js/api'
+import { AnyJson } from '@polkadot/types/types'
+import { HexString } from '@polkadot/util/types'
+
 type StateWasmResponse = {
   fed: string
   entertained: string
   rested: string
 }
+
+type Args = {
+  programId: HexString | undefined
+  wasm: Buffer | Uint8Array | undefined
+  programMetadata: ProgramMetadata | undefined
+  functionName: string | undefined
+  payload?: AnyJson
+  argument?: AnyJson
+}
+
+// TODO: monkey patch, since we need to re-read state every few seconds
+// get rid after @gear-js/react-hooks update to return readState callback
+function useReadWasmState<T = AnyJson>(args: Args, isReadOnError?: boolean) {
+  const { programId, wasm, programMetadata, functionName, payload, argument } =
+    args
+
+  const { api } = useApi()
+  const alert = useAlert()
+
+  const [state, setState] = useState<T>()
+  const [isStateRead, setIsStateRead] = useState(true)
+  const [error, setError] = useState('')
+
+  const isPayload = payload !== undefined
+  const isArgument = argument !== undefined
+
+  const readWasmState = (isInitLoad?: boolean) => {
+    if (
+      !api ||
+      !programId ||
+      !wasm ||
+      !programMetadata ||
+      !functionName ||
+      !isArgument ||
+      !isPayload
+    )
+      return
+
+    if (isInitLoad) setIsStateRead(false)
+
+    getStateMetadata(wasm)
+      .then((stateMetadata) =>
+        api.programState.readUsingWasm(
+          { programId, wasm, fn_name: functionName, argument, payload },
+          stateMetadata,
+          programMetadata
+        )
+      )
+      .then((codecState) => codecState.toHuman())
+      .then((result) => {
+        setState(result as unknown as T)
+        if (!isReadOnError) setIsStateRead(true)
+      })
+      .catch(({ message }: Error) => setError(message))
+      .finally(() => {
+        if (isReadOnError) setIsStateRead(true)
+      })
+  }
+
+  const handleStateChange = ({ data }: MessagesDispatched) => {
+    const changedIDs = data.stateChanges.toHuman() as HexString[]
+    const isAnyChange = changedIDs.some((id) => id === programId)
+
+    if (isAnyChange) readWasmState()
+  }
+
+  useEffect(() => {
+    if (
+      !api ||
+      !programId ||
+      !wasm ||
+      !programMetadata ||
+      !functionName ||
+      !isArgument ||
+      !isPayload
+    )
+      return
+
+    const unsub = api.gearEvents.subscribeToGearEvent(
+      'MessagesDispatched',
+      handleStateChange
+    )
+
+    return () => {
+      unsub.then((unsubCallback) => unsubCallback())
+    }
+  }, [api, programId, wasm, programMetadata, functionName, argument, payload])
+
+  useEffect(() => {
+    readWasmState(true)
+    setError('')
+  }, [api, programId, wasm, programMetadata, functionName, argument, payload])
+
+  useEffect(() => {
+    if (error) alert.error(error)
+  }, [error])
+
+  // contract calculates new state on read without MessagesDispatched events
+  useEffect(() => {
+    if (!state) return
+
+    const interval = setInterval(() => readWasmState(), 10000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [state])
+
+  return { state, isStateRead, error, readWasmState }
+}
+
+export { useReadWasmState }
 
 export function useThrottleWasmState() {
   const { lesson, setIsReady, isReady } = useLessons()
@@ -19,7 +139,7 @@ export function useThrottleWasmState() {
   const [, meta] = useLessonAssets()
   const { tamagotchi, setTamagotchi } = useTamagotchi()
 
-  const { state } = useReadWasmState<StateWasmResponse>({
+  const { state, readWasmState } = useReadWasmState<StateWasmResponse>({
     programId: lesson?.programId,
     wasm: stateMeta?.buffer,
     programMetadata: meta,
