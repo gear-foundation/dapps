ad72d4ab434e35363255f0c6c541cec6221ea775
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-01 15:09:50 +0300

    Fix build (#77)

--- frontend/battleship/src/features/game/components/game-process/game-process.tsx
+++ frontend/battleship/src/features/game/components/game-process/game-process.tsx
@@ -1,112 +1,100 @@
-import { useEffect, useState } from 'react'
-import { Text } from '@/components/ui/text'
-import { GameEndModal, Map } from '@/features/game'
+import { useEffect, useState } from 'react';
+import { Text } from '@/components/ui/text';
+import { GameEndModal, Map } from '@/features/game';
 
-import styles from './GameProcess.module.scss'
-import { MapEnemy } from '../map'
-import { useGame, useGameMessage, usePending } from '../../hooks'
-import { getFormattedTime } from '../../utils'
-import { Loader } from '@/components'
-import { useAccount } from '@gear-js/react-hooks'
-import { useFetchVoucher } from '@/app/hooks/useFetchVoucher'
+import styles from './GameProcess.module.scss';
+import { MapEnemy } from '../map';
+import { useGame, useGameMessage, usePending } from '../../hooks';
+import { getFormattedTime } from '../../utils';
+import { Loader } from '@/components';
+import { useAccount } from '@gear-js/react-hooks';
+import { useFetchVoucher } from '@/app/hooks/useFetchVoucher';
 
 export default function GameProcess() {
-  const { account } = useAccount()
-  const { isVoucher, isLoading, updateBalance } = useFetchVoucher(
-    account?.address
-  )
+  const { account } = useAccount();
+  const { isVoucher, isLoading, updateBalance } = useFetchVoucher(account?.address);
 
-  const [canExecute, setCanExecute] = useState(false)
-  const [playerShips, setPlayerShips] = useState<string[]>([])
-  const [enemiesShips, setEnemiesShips] = useState<string[]>([])
-  const [elapsedTime, setElapsedTime] = useState('')
-  const [totalGameTime, setTotalGameTime] = useState('')
+  const [canExecute, setCanExecute] = useState(false);
+  const [playerShips, setPlayerShips] = useState<string[]>([]);
+  const [enemiesShips, setEnemiesShips] = useState<string[]>([]);
+  const [elapsedTime, setElapsedTime] = useState('');
+  const [totalGameTime, setTotalGameTime] = useState('');
 
-  const { gameState } = useGame()
-  const { setPending } = usePending()
-  const message = useGameMessage()
+  const { gameState } = useGame();
+  const { setPending } = usePending();
+  const message = useGameMessage();
 
-  const [isOpenEndModal, setIsOpenEndModal] = useState(false)
-  const openEndModal = () => setIsOpenEndModal(true)
-  const closeEndModal = () => setIsOpenEndModal(false)
+  const [isOpenEndModal, setIsOpenEndModal] = useState(false);
+  const openEndModal = () => setIsOpenEndModal(true);
+  const closeEndModal = () => setIsOpenEndModal(false);
 
-  const totalShips = gameState?.botShips.reduce(
-    (total, [shipType, shipCount]) => {
-      return total + parseInt(shipCount, 10)
-    },
-    0
-  )
-  const totalShoots = gameState ? parseInt(gameState.totalShots) : 0
-  const successfulShoots =
-    gameState?.botBoard.filter((x) => x === 'DeadShip' || x === 'BoomShip')
-      .length ?? 0
-  const efficiency =
-    totalShoots !== 0 ? ((successfulShoots / totalShoots) * 100).toFixed(2) : 0
+  const totalShips = gameState?.botShips.reduce((total, [shipType, shipCount]) => {
+    return total + parseInt(shipCount, 10);
+  }, 0);
+  const totalShoots = gameState ? parseInt(gameState.totalShots) : 0;
+  const successfulShoots = gameState?.botBoard.filter((x) => x === 'DeadShip' || x === 'BoomShip').length ?? 0;
+  const efficiency = totalShoots !== 0 ? ((successfulShoots / totalShoots) * 100).toFixed(2) : 0;
 
   useEffect(() => {
     if (gameState) {
-      let gameOverHandled = false
+      let gameOverHandled = false;
 
       const updateTimer = () => {
-        const currentTime = new Date().getTime()
-        const startTime = parseInt(gameState.startTime.replace(/,/g, ''))
-        const elapsedTimeMilliseconds = currentTime - startTime
+        const currentTime = new Date().getTime();
+        const startTime = parseInt(gameState.startTime.replace(/,/g, ''));
+        const elapsedTimeMilliseconds = currentTime - startTime;
 
-        const formattedTime = getFormattedTime(elapsedTimeMilliseconds)
+        const formattedTime = getFormattedTime(elapsedTimeMilliseconds);
 
-        !gameState.gameOver && setElapsedTime(formattedTime)
+        !gameState.gameOver && setElapsedTime(formattedTime);
 
         if (gameState.gameOver && !gameOverHandled) {
-          const endTime = parseInt(gameState.endTime.replace(/,/g, ''))
-          const elapsedTimeMilliseconds = endTime - startTime
-          const formattedTime = getFormattedTime(elapsedTimeMilliseconds)
+          const endTime = parseInt(gameState.endTime.replace(/,/g, ''));
+          const elapsedTimeMilliseconds = endTime - startTime;
+          const formattedTime = getFormattedTime(elapsedTimeMilliseconds);
 
-          setElapsedTime(formattedTime)
-          setTotalGameTime(formattedTime)
-          openEndModal()
+          setElapsedTime(formattedTime);
+          setTotalGameTime(formattedTime);
+          openEndModal();
 
-          gameOverHandled = true
+          gameOverHandled = true;
         }
-      }
+      };
 
-      const timerInterval = setInterval(updateTimer, 1000)
+      const timerInterval = setInterval(updateTimer, 1000);
 
       return () => {
-        clearInterval(timerInterval)
-      }
+        clearInterval(timerInterval);
+      };
     }
-  }, [gameState])
+  }, [gameState]);
 
   const onClickCell = async (indexCell: number) => {
-    await updateBalance()
+    await updateBalance();
     if (!isLoading) {
       message({
-        payload: {
-          Turn: {
-            step: indexCell,
-          },
-        },
+        payload: { Turn: { step: indexCell } },
         onSuccess: () => {
-          setPending(false)
-          setCanExecute(true)
+          setPending(false);
+          setCanExecute(true);
         },
         withVoucher: isVoucher,
-      })
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (gameState?.playerBoard) {
-      setPlayerShips(gameState.playerBoard)
+      setPlayerShips(gameState.playerBoard);
     }
 
     if (gameState?.botBoard) {
-      setEnemiesShips(gameState.botBoard)
+      setEnemiesShips(gameState.botBoard);
     }
-  }, [gameState])
+  }, [gameState]);
 
   if (!gameState) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
@@ -137,10 +125,10 @@ export default function GameProcess() {
 
         <div className={styles.listShips}>
           {gameState?.botShips.map(([shipType, shipCount], index) => {
-            const numberOfBlocks = parseInt(shipType, 10)
+            const numberOfBlocks = parseInt(shipType, 10);
             const shipsToRender = Array.from({
               length: parseInt(shipCount, 10),
-            })
+            });
 
             return (
               <div key={index} className={styles.ship}>
@@ -152,17 +140,12 @@ export default function GameProcess() {
                   </div>
                 ))}
               </div>
-            )
+            );
           })}
         </div>
       </div>
       <div>
-        <MapEnemy
-          sizeBlock={68}
-          onClickCell={onClickCell}
-          canExecute={canExecute}
-          shipStatusArray={enemiesShips}
-        />
+        <MapEnemy sizeBlock={68} onClickCell={onClickCell} canExecute={canExecute} shipStatusArray={enemiesShips} />
       </div>
 
       {isOpenEndModal && gameState && (
@@ -176,5 +159,5 @@ export default function GameProcess() {
         />
       )}
     </div>
-  )
+  );
 }
