54d426e55495385ef9f80a8b6502ea709c814183
yuriy <yuriy.faizov@gmail.com>
2023-11-02 14:45:35 +0300

    Battleship frontend: Add console log address game&node

--- frontend/battleship/src/app.tsx
+++ frontend/battleship/src/app.tsx
@@ -7,6 +7,7 @@ import { Header } from '@/components/layout'
 import { withProviders } from '@/app/hocs'
 
 import { useWalletSync } from '@/features/wallet/hooks'
+import { ADDRESS } from '@/app/consts'
 
 function Component() {
   const { isApiReady } = useApi()
@@ -15,6 +16,9 @@ function Component() {
   useWalletSync()
 
   const isAppReady = isApiReady && isAccountReady
+
+  console.log('ADDRESS NODE:', ADDRESS.NODE)
+  console.log('ADDRESS GAME:', ADDRESS.GAME)
 
   return (
     <main>
