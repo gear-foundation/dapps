8f714247f03f9cbb572036ea16793d2536943ca4
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-15 18:05:48 +0300

    Fix tamagotchi battle build

--- frontend/apps/tamagotchi-battle/src/index.tsx
+++ frontend/apps/tamagotchi-battle/src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TagManager from 'react-gtm-module';
 import { initErrorTracking, logPublicEnvs } from '@dapps-frontend/error-tracking';
 import { App } from './app';
-
-if (process.env.MODE === 'production' && process.env.VITE_GTM_ID_TTT) {
-  TagManager.initialize({
-    gtmId: process.env.VITE_GTM_ID_TTT,
-  });
-}
 
 initErrorTracking();
 logPublicEnvs();
