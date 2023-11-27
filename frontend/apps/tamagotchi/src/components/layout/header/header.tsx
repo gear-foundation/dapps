741fd01a4a31adb8a1ee94f3f65483d9831be37c
Nikita Yutanov <nikitayutanov@gmail.com>
2023-11-27 15:13:29 +0300

    Fix tamagotchi logo

--- frontend/apps/tamagotchi/src/components/layout/header/header.tsx
+++ frontend/apps/tamagotchi/src/components/layout/header/header.tsx
@@ -1,21 +1,13 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Wallet } from '@dapps-frontend/ui';
 import { SpriteIcon } from '@/components/ui/sprite-icon';
 
 export const Header = () => {
-  const { pathname } = useLocation();
-
   return (
     <header className="container flex justify-between items-center py-7.5">
-      {pathname !== '/' ? (
-        <Link to="/" className="inline-flex text-white transition-colors hover:text-opacity-70">
-          <SpriteIcon name="logo" width={180} height={44} className="h-10" />
-        </Link>
-      ) : (
-        <span className="inline-flex text-white">
-          <SpriteIcon name="logo" width={180} height={44} className="h-10" />
-        </span>
-      )}
+      <Link to="/" className="inline-flex text-white transition-colors hover:text-opacity-70">
+        <SpriteIcon name="logo" width={180} height={44} className="h-10" />
+      </Link>
 
       <Wallet />
     </header>
