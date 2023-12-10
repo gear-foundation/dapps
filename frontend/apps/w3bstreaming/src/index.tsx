7da3d27a465786be97a1198818278dfcfc297066
Yauheni <avegen.dr@gmail.com>
2023-12-10 23:35:32 +0300

    Adds package inits to index

--- frontend/apps/w3bstreaming/src/index.tsx
+++ frontend/apps/w3bstreaming/src/index.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { initErrorTracking, logPublicEnvs, initAnalytics } from '@dapps-frontend/error-tracking';
 import { App } from '@/App';
-
 import './styles/global.scss';
+
+initAnalytics();
+initErrorTracking();
+logPublicEnvs();
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
