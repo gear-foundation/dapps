bf16bb45ba3f70b1448bf5567699284b34c81b3d
YauheniDraichykau <64776571+YauheniDraichykau@users.noreply.github.com>
2023-12-11 18:22:41 +0300

    Packages: Removes extra plugin in gasless transactions (#158)
    Co-authored-by: Yauheni <avegen.dr@gmail.com>

--- frontend/packages/gasless-transactions/vite.config.ts
+++ frontend/packages/gasless-transactions/vite.config.ts
@@ -2,11 +2,10 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), nodePolyfills()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
