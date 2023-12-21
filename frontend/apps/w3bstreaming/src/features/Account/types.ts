7282bb4ed09713194544d0a676ed6b30a08b596e
Yauheni <avegen.dr@gmail.com>
2023-12-21 12:12:12 +0300

    Streaming: Adds timezones

--- frontend/apps/w3bstreaming/src/features/Account/types.ts
+++ frontend/apps/w3bstreaming/src/features/Account/types.ts
@@ -12,6 +12,7 @@ export interface User {
   subscribers: string[];
   subscriptions: SubscriptionUser[];
   role: string;
+  timezone: string;
 }
 
 export interface UsersRes {
--- frontend/apps/w3bstreaming/src/ui/Select/Select.interfaces.ts
+++ frontend/apps/w3bstreaming/src/ui/Select/Select.interfaces.ts
@@ -3,4 +3,8 @@ import { SelectHTMLAttributes } from 'react';
 export interface SelectProps extends Omit<SelectHTMLAttributes<HTMLSelectElement>, 'size'> {
   size?: 'large' | 'medium' | 'small';
   label?: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
 }
