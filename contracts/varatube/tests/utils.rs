f91137345213fd95f34205752fc593948c46494c
LouiseMedova <luisa.medova@gmail.com>
2024-01-11 14:36:49 +0300

    clippy

--- contracts/varatube/tests/utils.rs
+++ contracts/varatube/tests/utils.rs
@@ -97,7 +97,7 @@ pub trait VaratubeTestFuncs {
 
 impl VaratubeTestFuncs for Program<'_> {
     fn varatube(system: &System, from: u64, config: Config) -> Program<'_> {
-        let varatube = Program::current(&system);
+        let varatube = Program::current(system);
         let res = varatube.send(from, config);
         assert!(!res.main_failed());
         varatube
