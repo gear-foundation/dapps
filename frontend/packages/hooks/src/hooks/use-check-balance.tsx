63af134af2d0705423bafea76a6341a07a8a9af0
Yauheni <avegen.dr@gmail.com>
2024-01-31 19:50:28 +0300

    Updates hooks package

---
+++ frontend/packages/hooks/src/hooks/use-check-balance.tsx
@@ -0,0 +1,52 @@
+import {
+  useAccount,
+  useAlert,
+  useApi,
+  useBalance,
+  useBalanceFormat,
+  useVouchers,
+  withoutCommas,
+} from '@gear-js/react-hooks';
+import { useSignlessTransactions } from '@dapps-frontend/signless-transactions';
+import { HexString, decodeAddress } from '@gear-js/api';
+import { stringShorten } from '@polkadot/util';
+
+function useCheckBalance(programId: HexString) {
+  const { api } = useApi();
+  const { account } = useAccount();
+  const { vouchers, isEachVoucherReady } = useVouchers(account?.decodedAddress, programId);
+  const { pair } = useSignlessTransactions();
+  const voucherAddress = pair ? decodeAddress(pair.address) : account?.decodedAddress;
+  const voucherKeys = isEachVoucherReady && vouchers ? Object.keys(vouchers) : [];
+  const firstVoucherKey = voucherKeys[0] as `0x${string}`;
+  const { balance } = useBalance(vouchers && voucherKeys.length ? vouchers[firstVoucherKey].owner : voucherAddress);
+  const { getFormattedBalanceValue, getFormattedGasValue } = useBalanceFormat();
+  const alert = useAlert();
+
+  const checkBalance = (limit: number, callback: () => void, onError?: () => void) => {
+    const chainBalance = Number(getFormattedBalanceValue(Number(withoutCommas(balance?.toString() || ''))).toFixed());
+    const valuePerGas = Number(withoutCommas(api!.valuePerGas!.toHuman()));
+    const chainEDeposit = Number(
+      getFormattedBalanceValue(Number(withoutCommas(api?.existentialDeposit.toString() || ''))).toFixed(),
+    );
+    const gasLimit = Number(getFormattedGasValue(limit).toFixed());
+
+    const chainEDepositWithLimit = chainEDeposit + gasLimit * valuePerGas;
+
+    if (chainBalance < chainEDepositWithLimit) {
+      alert.error(`Low balance on ${stringShorten(account?.decodedAddress || '', 8)}`);
+
+      if (onError) {
+        onError();
+      }
+
+      return;
+    }
+
+    callback();
+  };
+
+  return { checkBalance };
+}
+
+export { useCheckBalance };
