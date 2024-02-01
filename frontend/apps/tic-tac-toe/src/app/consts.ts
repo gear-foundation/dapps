87f24da4eddf4cb34cc35d1fce92538255f06349
Yauheni Draichykau <ichiru3333@gmail.com>
2024-02-01 14:26:41 +0100

    TicTacToe: decreasing polkadot util crypto version to 12.3.2

--- frontend/apps/tic-tac-toe/src/app/consts.ts
+++ frontend/apps/tic-tac-toe/src/app/consts.ts
@@ -4,7 +4,6 @@ export const ADDRESS = {
   NODE: import.meta.env.VITE_NODE_ADDRESS,
   BASE_NODES: import.meta.env.VITE_DEFAULT_NODES_URL,
   STAGING_NODES: import.meta.env.VITE_STAGING_NODES_URL,
-  GAME_STATE_SOCKET: 'wss://state-machine.vara-network.io',
   SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN_TTT,
 };
 
