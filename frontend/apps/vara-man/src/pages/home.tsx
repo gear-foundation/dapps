231813850ce84ac7f3328bf40163cd0e252efbac
Yuriy <43875549+faizov@users.noreply.github.com>
2024-03-18 18:26:30 +0300

    Fy/varaman single game (#285)

--- frontend/apps/vara-man/src/pages/home.tsx
+++ frontend/apps/vara-man/src/pages/home.tsx
@@ -1,12 +1,5 @@
 import { HomeRegister } from '@/components/sections/home/home-register';
-import { HomeNotActive } from '@/components/sections/home/home-not-active';
-import { useGame } from '@/app/context/ctx-game';
-import Game from './game';
-import { useInitGame } from '@/app/hooks/use-game';
-
 
 export default function Home() {
-  const { singleGame, tournamentGame } = useGame()
-
-  return <>{singleGame || tournamentGame ? <Game /> : <HomeRegister />}</>;
+  return <><HomeRegister /></>;
 }
