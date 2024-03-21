9af525a93539995aade2f16fd6a05bd58b4d9863
Yuriy <43875549+faizov@users.noreply.github.com>
2024-03-21 10:52:46 +0300

    Fy/varaman tournaments (#287)

--- frontend/apps/vara-man/src/app/utils/index.ts
+++ frontend/apps/vara-man/src/app/utils/index.ts
@@ -1,77 +1,103 @@
-import { ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import { AlertContainerFactory } from '@gear-js/react-hooks';
-import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
-import { LOCAL_STORAGE } from '@/app/consts';
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { AlertContainerFactory } from '@gear-js/react-hooks'
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types'
+import { isHex } from '@polkadot/util'
+import { LOCAL_STORAGE } from '@/app/consts'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs))
 }
 
 export function formatDate(input: string | number): string {
-  const date = new Date(input);
-  return date.toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  });
+	const date = new Date(input)
+	return date.toLocaleDateString('en-US', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
+	})
 }
 
 export function prettyDate(
-  input: number | Date | string,
-  options: Intl.DateTimeFormatOptions = {
-    dateStyle: 'long',
-    timeStyle: 'short',
-    hourCycle: 'h23',
-  },
-  locale: string = 'en-US',
+	input: number | Date | string,
+	options: Intl.DateTimeFormatOptions = {
+		dateStyle: 'long',
+		timeStyle: 'short',
+		hourCycle: 'h23',
+	},
+	locale: string = 'en-US'
 ) {
-  const date = typeof input === 'string' ? new Date(input) : input;
-  return new Intl.DateTimeFormat(locale, options).format(date);
+	const date = typeof input === 'string' ? new Date(input) : input
+	return new Intl.DateTimeFormat(locale, options).format(date)
 }
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+	return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
 }
 
-export const sleep = (s: number) => new Promise((resolve) => setTimeout(resolve, s * 1000));
+export const sleep = (s: number) =>
+	new Promise((resolve) => setTimeout(resolve, s * 1000))
 
 export const copyToClipboard = async ({
-  alert,
-  key,
-  successfulText,
+	alert,
+	key,
+	successfulText,
 }: {
-  alert: AlertContainerFactory;
-  key: string;
-  successfulText?: string;
+	alert: AlertContainerFactory
+	key: string
+	successfulText?: string
 }) => {
-  const onSuccess = () => alert.success(successfulText || 'Copied');
-  const onError = () => alert.error('Copy error');
+	const onSuccess = () => alert.success(successfulText || 'Copied')
+	const onError = () => alert.error('Copy error')
 
-  function unsecuredCopyToClipboard(text: string) {
-    const textArea = document.createElement('textarea');
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    try {
-      document.execCommand('copy');
-      onSuccess();
-    } catch (err) {
-      console.error('Unable to copy to clipboard', err);
-      onError();
-    }
-    document.body.removeChild(textArea);
-  }
+	function unsecuredCopyToClipboard(text: string) {
+		const textArea = document.createElement('textarea')
+		textArea.value = text
+		document.body.appendChild(textArea)
+		textArea.focus()
+		textArea.select()
+		try {
+			document.execCommand('copy')
+			onSuccess()
+		} catch (err) {
+			console.error('Unable to copy to clipboard', err)
+			onError()
+		}
+		document.body.removeChild(textArea)
+	}
 
-  if (window.isSecureContext && navigator.clipboard) {
-    navigator.clipboard
-      .writeText(key)
-      .then(() => onSuccess())
-      .catch(() => onError());
-  } else {
-    unsecuredCopyToClipboard(key);
-  }
-};
+	if (window.isSecureContext && navigator.clipboard) {
+		navigator.clipboard
+			.writeText(key)
+			.then(() => onSuccess())
+			.catch(() => onError())
+	} else {
+		unsecuredCopyToClipboard(key)
+	}
+}
 
-export const isLoggedIn = ({ address }: InjectedAccountWithMeta) => localStorage[LOCAL_STORAGE.ACCOUNT] === address;
+export const isLoggedIn = ({ address }: InjectedAccountWithMeta) =>
+	localStorage[LOCAL_STORAGE.ACCOUNT] === address
+
+export const hexRequired = (value: string) =>
+	!value
+		? 'Field is required'
+		: !isHex(value)
+		? 'String must be in Hex format'
+		: null
+
+export type Validator = (value: any) => 'Field is required' | null
+
+export const numberRequired: Validator = (value) => {
+	if (value === null || value === undefined || typeof value !== 'number') {
+		return 'Field is required'
+	}
+	return null
+}
+
+export const stringRequired: Validator = (value) => {
+	if (typeof value !== 'string' || value.trim() === '') {
+		return 'Field is required'
+	}
+	return null
+}
--- frontend/apps/vara-man/src/pages/game.tsx
+++ frontend/apps/vara-man/src/pages/game.tsx
@@ -1,9 +1,13 @@
-import { Game } from '@/feature/game/';
+import { Game as TournamentGame } from '@/feature/tournament-game';
+import { Game as SingleGame } from '@/feature/single-game'
+import { useGame } from '@/app/context/ctx-game';
 
 export default function GamePage() {
+  const { tournamentGame, previousGame } = useGame()
+
   return (
     <>
-      <Game />
+      {tournamentGame || previousGame ? <TournamentGame /> : <SingleGame />}
     </>
   );
 }
