a87c694b3277bf9330f595a6ab9f8cdaece8ca15
YauheniDraichykau <64776571+YauheniDraichykau@users.noreply.github.com>
2024-03-21 11:56:27 +0300

    Yd/syndote updates (#288)
    Co-authored-by: Yauheni <avegen.dr@gmail.com>

--- frontend/apps/syndote/src/App.tsx
+++ frontend/apps/syndote/src/App.tsx
@@ -3,13 +3,24 @@ import { Routing } from 'pages';
 import { ApiLoader } from 'components';
 import { withProviders } from 'hocs';
 import 'simplebar-react/dist/simplebar.min.css';
+import '@gear-js/vara-ui/dist/style.css';
 import 'App.scss';
+import { Header } from 'components/layout/header';
+import { Container, Footer } from '@dapps-frontend/ui';
 
 function Component() {
   const { isApiReady } = useApi();
   const { isAccountReady } = useAccount();
 
-  return <main>{isApiReady && isAccountReady ? <Routing /> : <ApiLoader />}</main>;
+  return (
+    <>
+      <Header />
+      <main>{isApiReady && isAccountReady ? <Routing /> : <ApiLoader />}</main>
+      <Container>
+        <Footer vara />
+      </Container>
+    </>
+  );
 }
 
 export const App = withProviders(Component);
