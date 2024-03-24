99c6aafd3eac4dc353c05e2fc2ba157cf2c577d1
Yauheni <avegen.dr@gmail.com>
2024-03-24 15:30:08 +0300

    Streaming: fixes connect wallet button

--- frontend/apps/w3bstreaming/src/pages/MainPage/components/IntrodutionInfo/IntrodutionInfo.tsx
+++ frontend/apps/w3bstreaming/src/pages/MainPage/components/IntrodutionInfo/IntrodutionInfo.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { Button } from '@ui';
+import { WalletNew as Wallet } from '@dapps-frontend/ui';
 import styles from './IntrodutionInfo.module.scss';
 import mainFrame from '@/assets/icons/main-page-frame.png';
 import animImg from '@/assets/icons/main-page-wara-anim.png';
@@ -7,16 +6,6 @@ import courtain from '@/assets/icons/courtain.png';
 import { cx } from '@/utils';
 
 function IntrodutionInfo() {
-  const [isWalletModalOpen, setIsWalletModalOpen] = useState<boolean>(false);
-
-  const handleOpenWalletModal = () => {
-    setIsWalletModalOpen(true);
-  };
-
-  const handleCloseWalletModal = () => {
-    setIsWalletModalOpen(false);
-  };
-
   return (
     <div className={cx(styles.content)}>
       <div className={cx(styles.left)}>
@@ -28,14 +17,13 @@ function IntrodutionInfo() {
           Revolutionize media consumption with seamless audio and video streaming using P2P technology. Stream and share
           content directly from your devices, eliminating the need for centralized servers
         </p>
-        <Button label="Connect Wallet" variant="primary" size="large" onClick={handleOpenWalletModal} />
+        <Wallet />
       </div>
       <div className={cx(styles.right)}>
         <img src={mainFrame} alt="united people" className={cx(styles['lower-img'])} />
         <img src={courtain} alt="courtain" className={cx(styles.courtain)} />
         <img src={animImg} alt="some stuff" className={cx(styles['upper-img'])} />
       </div>
-      {/* <WalletModal open={isWalletModalOpen} setOpen={setIsWalletModalOpen} onClose={handleCloseWalletModal} /> */}
     </div>
   );
 }
