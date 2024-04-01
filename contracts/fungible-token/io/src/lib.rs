a380ddb136bb400c4e9b8c9733959066ad2bfd9b
LouiseMedova <luisa.medova@gmail.com>
2024-04-01 11:38:37 +0300

    grc20 implementation

--- contracts/fungible-token/io/src/lib.rs
+++ contracts/fungible-token/io/src/lib.rs
@@ -1,17 +1,19 @@
 #![no_std]
 
-use gmeta::{In, InOut, Metadata, Out};
+use gmeta::{In, InOut, Metadata};
 use gstd::{prelude::*, ActorId};
 
+pub type TxId = u64;
+pub type ValidUntil = u64;
 pub struct FungibleTokenMetadata;
 
 impl Metadata for FungibleTokenMetadata {
     type Init = In<InitConfig>;
-    type Handle = InOut<FTAction, FTEvent>;
+    type Handle = InOut<FTAction, Result<FTReply, FTError>>;
     type Others = ();
     type Reply = ();
     type Signal = ();
-    type State = Out<IoFungibleToken>;
+    type State = InOut<Query, QueryReply>;
 }
 
 #[derive(Debug, Decode, Encode, TypeInfo)]
@@ -21,53 +23,113 @@ pub struct InitConfig {
     pub name: String,
     pub symbol: String,
     pub decimals: u8,
+    pub initial_capacity: Option<u32>,
+    pub config: Config,
+}
+
+#[derive(Debug, Decode, Encode, TypeInfo, Default, Clone)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct Config {
+    pub tx_storage_period: u64,
+    pub tx_payment: u128,
 }
 
 #[derive(Debug, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum FTAction {
-    Mint(u128),
-    Burn(u128),
     Transfer {
+        tx_id: Option<TxId>,
         from: ActorId,
         to: ActorId,
         amount: u128,
     },
     Approve {
+        tx_id: Option<TxId>,
         to: ActorId,
         amount: u128,
     },
-    TotalSupply,
     BalanceOf(ActorId),
+    AddAdmin {
+        admin_id: ActorId,
+    },
+    DeleteAdmin {
+        admin_id: ActorId,
+    },
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub enum FTEvent {
-    Transfer {
+pub enum FTReply {
+    Transferred {
         from: ActorId,
         to: ActorId,
         amount: u128,
     },
-    Approve {
+    Approved {
         from: ActorId,
         to: ActorId,
         amount: u128,
     },
-    TotalSupply(u128),
+    AdminAdded {
+        admin_id: ActorId,
+    },
+    AdminRemoved {
+        admin_id: ActorId,
+    },
     Balance(u128),
 }
 
-#[derive(Debug, Clone, Default, Encode, Decode, TypeInfo)]
+#[derive(Debug, Encode, Decode, TypeInfo, MaxEncodedLen, Clone)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub struct IoFungibleToken {
-    pub name: String,
-    pub symbol: String,
-    pub total_supply: u128,
-    pub balances: Vec<(ActorId, u128)>,
-    pub allowances: Vec<(ActorId, Vec<(ActorId, u128)>)>,
-    pub decimals: u8,
+pub enum FTError {
+    NotAdmin,
+    NotEnoughBalance,
+    ZeroAddress,
+    NotAllowedToTransfer,
+    AdminAlreadyExists,
+    CantDeleteYourself,
+    TxAlreadyExists,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum Query {
+    Name,
+    Symbol,
+    Decimals,
+    BalanceOf(ActorId),
+    AllowanceOfAccount {
+        account: ActorId,
+        approved_account: ActorId,
+    },
+    Admins,
+    GetTxValidityTime {
+        account: ActorId,
+        tx_id: TxId,
+    },
+    GetTxIdsForAccount {
+        account: ActorId,
+    }
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum QueryReply {
+    Name(String),
+    Symbol(String),
+    Decimals(u8),
+    TotalSupply(u128),
+    Balance(u128),
+    AllowanceOfAccount(u128),
+    Admins(Vec<ActorId>),
+    TxValidityTime(ValidUntil),
+    TxIdsForAccount {
+        tx_ids: Vec<TxId>,
+    }
 }
--- contracts/fungible-token/src/lib.rs
+++ contracts/fungible-token/src/lib.rs
@@ -2,13 +2,14 @@
 
 use fungible_token_io::*;
 use gstd::{
-    collections::HashMap, errors::Result as GstdResult, msg, prelude::*, ActorId, MessageId,
+    collections::{hash_map::Entry, HashMap, HashSet},
+    exec, msg,
+    prelude::*,
+    ActorId,
 };
 
 #[cfg(test)]
 mod tests;
-
-const ZERO_ID: ActorId = ActorId::new([0u8; 32]);
 
 #[derive(Debug, Clone, Default)]
 struct FungibleToken {
@@ -16,70 +17,75 @@ struct FungibleToken {
     name: String,
     /// Symbol of the token.
     symbol: String,
-    /// Total supply of the token.
-    total_supply: u128,
+    /// Token's decimals.
+    decimals: u8,
     /// Map to hold balances of token holders.
     balances: HashMap<ActorId, u128>,
     /// Map to hold allowance information of token holders.
     allowances: HashMap<ActorId, HashMap<ActorId, u128>>,
-    /// Token's decimals.
-    pub decimals: u8,
+    /// Mapping of executed transactions to the time they are valid.
+    tx_ids: HashMap<(ActorId, TxId), ValidUntil>,
+    /// Mapping of accounts to their transaction IDs.
+    account_to_tx_ids: HashMap<ActorId, HashSet<TxId>>,
+    /// Configuration parameters for the fungible token contract.
+    config: Config,
+    admins: Vec<ActorId>,
 }
 
 static mut FUNGIBLE_TOKEN: Option<FungibleToken> = None;
 
 impl FungibleToken {
-    /// Executed on receiving `fungible-token-messages::MintInput`.
-    fn mint(&mut self, amount: u128) {
+    fn add_admin(&mut self, admin_id: &ActorId) -> Result<FTReply, FTError> {
         let source = msg::source();
-        self.balances
-            .entry(source)
-            .and_modify(|balance| *balance += amount)
-            .or_insert(amount);
-        self.total_supply += amount;
-        msg::reply(
-            FTEvent::Transfer {
-                from: ZERO_ID,
-                to: source,
-                amount,
-            },
-            0,
-        )
-        .unwrap();
-    }
-    /// Executed on receiving `fungible-token-messages::BurnInput`.
-    fn burn(&mut self, amount: u128) {
-        let source = msg::source();
-        if self.balances.get(&source).unwrap_or(&0) < &amount {
-            panic!("Amount exceeds account balance");
+        if !self.admins.contains(&source) {
+            return Err(FTError::NotAdmin);
         }
-        self.balances
-            .entry(source)
-            .and_modify(|balance| *balance -= amount);
-        self.total_supply -= amount;
+        if self.admins.contains(admin_id) {
+            return Err(FTError::AdminAlreadyExists);
+        }
+        self.admins.push(*admin_id);
+        Ok(FTReply::AdminAdded {
+            admin_id: *admin_id,
+        })
+    }
 
-        msg::reply(
-            FTEvent::Transfer {
-                from: source,
-                to: ZERO_ID,
-                amount,
-            },
-            0,
-        )
-        .unwrap();
+    fn delete_admin(&mut self, admin_id: &ActorId) -> Result<FTReply, FTError> {
+        let source = msg::source();
+        if !self.admins.contains(&source) {
+            return Err(FTError::NotAdmin);
+        }
+
+        if admin_id == &source {
+            return Err(FTError::CantDeleteYourself);
+        }
+
+        self.admins.retain(|acc| acc != admin_id);
+        Ok(FTReply::AdminRemoved {
+            admin_id: *admin_id,
+        })
     }
-    /// Executed on receiving `fungible-token-messages::TransferInput` or `fungible-token-messages::TransferFromInput`.
-    /// Transfers `amount` tokens from `sender` account to `recipient` account.
-    fn transfer(&mut self, from: &ActorId, to: &ActorId, amount: u128) {
-        if from == &ZERO_ID || to == &ZERO_ID {
-            panic!("Zero addresses");
+    fn transfer(
+        &mut self,
+        tx_id: Option<TxId>,
+        from: &ActorId,
+        to: &ActorId,
+        amount: u128,
+    ) -> Result<FTReply, FTError> {
+        let msg_source = msg::source();
+        let block_timestamp = exec::block_timestamp();
+        if let Some(tx_id) = tx_id {
+            self.clear_outdated_tx_ids(&msg_source, block_timestamp);
+            self.check_tx_id(tx_id, &msg_source)?;
+        }
+
+        if *from == ActorId::zero() || *to == ActorId::zero() {
+            return Err(FTError::ZeroAddress);
         };
-        if !self.can_transfer(from, amount) {
-            panic!("Not allowed to transfer")
-        }
-        if self.balances.get(from).unwrap_or(&0) < &amount {
-            panic!("Amount exceeds account balance");
-        }
+
+        self.check_balance(&from, amount)?;
+
+        self.can_transfer(&msg_source, &from, amount)?;
+
         self.balances
             .entry(*from)
             .and_modify(|balance| *balance -= amount);
@@ -87,152 +93,212 @@ impl FungibleToken {
             .entry(*to)
             .and_modify(|balance| *balance += amount)
             .or_insert(amount);
-        msg::reply(
-            FTEvent::Transfer {
-                from: *from,
-                to: *to,
-                amount,
-            },
-            0,
-        )
-        .unwrap();
+
+        self.set_tx_id_status(
+            tx_id,
+            &msg_source,
+            block_timestamp + self.config.tx_storage_period,
+        );
+
+        Ok(FTReply::Transferred {
+            from: *from,
+            to: *to,
+            amount,
+        })
     }
 
     /// Executed on receiving `fungible-token-messages::ApproveInput`.
-    fn approve(&mut self, to: &ActorId, amount: u128) {
-        if to == &ZERO_ID {
-            panic!("Approve to zero address");
+    fn approve(
+        &mut self,
+        tx_id: Option<TxId>,
+        to: &ActorId,
+        amount: u128,
+    ) -> Result<FTReply, FTError> {
+        if *to == ActorId::zero() {
+            return Err(FTError::ZeroAddress);
         }
-        let source = msg::source();
+        let msg_source = msg::source();
+        let block_timestamp = exec::block_timestamp();
+        if let Some(tx_id) = tx_id {
+            self.clear_outdated_tx_ids(&msg_source, block_timestamp);
+            self.check_tx_id(tx_id, &msg_source)?;
+        }
         self.allowances
-            .entry(source)
+            .entry(msg_source)
             .or_default()
             .insert(*to, amount);
-        msg::reply(
-            FTEvent::Approve {
-                from: source,
-                to: *to,
-                amount,
-            },
-            0,
-        )
-        .unwrap();
+        self.set_tx_id_status(
+            tx_id,
+            &msg_source,
+            block_timestamp + self.config.tx_storage_period,
+        );
+        Ok(FTReply::Approved {
+            from: msg_source,
+            to: *to,
+            amount,
+        })
     }
 
-    fn can_transfer(&mut self, from: &ActorId, amount: u128) -> bool {
-        let source = msg::source();
-        if from == &source && self.balances.get(&source).unwrap_or(&0) >= &amount {
-            return true;
+    fn check_balance(&self, account: &ActorId, amount: u128) -> Result<(), FTError> {
+        if *self.balances.get(account).unwrap_or(&0) < amount {
+            return Err(FTError::NotEnoughBalance);
         }
-        if let Some(allowed_amount) = self.allowances.get(from).and_then(|m| m.get(&source)) {
-            if allowed_amount >= &amount {
-                self.allowances.entry(*from).and_modify(|m| {
-                    m.entry(source).and_modify(|a| *a -= amount);
-                });
-                return true;
+        Ok(())
+    }
+
+    fn can_transfer(
+        &mut self,
+        source: &ActorId,
+        from: &ActorId,
+        amount: u128,
+    ) -> Result<(), FTError> {
+        if from != source {
+            if let Some(allowed_amount) = self.allowances.get(from).and_then(|m| m.get(source)) {
+                if allowed_amount >= &amount {
+                    self.allowances.entry(*from).and_modify(|m| {
+                        m.entry(*source).and_modify(|a| *a -= amount);
+                    });
+                } else {
+                    return Err(FTError::NotAllowedToTransfer);
+                }
+            } else {
+                return Err(FTError::NotAllowedToTransfer);
             }
         }
-        false
+        Ok(())
     }
-}
 
-fn common_state() -> IoFungibleToken {
-    let state = static_mut_state();
-    let FungibleToken {
-        name,
-        symbol,
-        total_supply,
-        balances,
-        allowances,
-        decimals,
-    } = state.clone();
-
-    let balances = balances.iter().map(|(k, v)| (*k, *v)).collect();
-    let allowances = allowances
-        .iter()
-        .map(|(id, allowance)| (*id, allowance.iter().map(|(k, v)| (*k, *v)).collect()))
-        .collect();
-    IoFungibleToken {
-        name,
-        symbol,
-        total_supply,
-        balances,
-        allowances,
-        decimals,
+    fn set_tx_id_status(
+        &mut self,
+        tx_id: Option<TxId>,
+        account: &ActorId,
+        valid_until: ValidUntil,
+    ) {
+        if let Some(tx_id) = tx_id {
+            self.tx_ids.insert((*account, tx_id), valid_until);
+        }
     }
-}
 
-fn static_mut_state() -> &'static mut FungibleToken {
-    unsafe { FUNGIBLE_TOKEN.get_or_insert(Default::default()) }
-}
+    fn check_tx_id(&self, tx_id: TxId, account: &ActorId) -> Result<(), FTError> {
+        if self.tx_ids.get(&(*account, tx_id)).is_some() {
+            return Err(FTError::TxAlreadyExists);
+        }
 
-#[no_mangle]
-extern fn state() {
-    reply(common_state())
-        .expect("Failed to encode or reply with `<AppMetadata as Metadata>::State` from `state()`");
-}
+        Ok(())
+    }
 
-fn reply(payload: impl Encode) -> GstdResult<MessageId> {
-    msg::reply(payload, 0)
+    fn clear_outdated_tx_ids(&mut self, account: &ActorId, block_timestamp: u64) {
+        if let Entry::Occupied(mut tx_ids) = self.account_to_tx_ids.entry(*account) {
+            let tx_ids_cloned = tx_ids.get().clone();
+            for tx_id in tx_ids_cloned {
+                let valid_until = self.tx_ids.get(&(*account, tx_id)).expect("Cant be None");
+                if block_timestamp > *valid_until {
+                    self.tx_ids.remove(&(*account, tx_id));
+                    tx_ids.get_mut().remove(&tx_id);
+                }
+            }
+            if tx_ids.get().is_empty() {
+                tx_ids.remove_entry();
+            }
+        }
+    }
 }
 
 #[no_mangle]
 extern fn handle() {
     let action: FTAction = msg::load().expect("Could not load Action");
-    let ft: &mut FungibleToken = unsafe { FUNGIBLE_TOKEN.get_or_insert(Default::default()) };
-    match action {
-        FTAction::Mint(amount) => {
-            ft.mint(amount);
-        }
-        FTAction::Burn(amount) => {
-            ft.burn(amount);
-        }
-        FTAction::Transfer { from, to, amount } => {
-            ft.transfer(&from, &to, amount);
-        }
-        FTAction::Approve { to, amount } => {
-            ft.approve(&to, amount);
-        }
-        FTAction::TotalSupply => {
-            msg::reply(FTEvent::TotalSupply(ft.total_supply), 0).unwrap();
-        }
+    let ft: &mut FungibleToken = unsafe {
+        FUNGIBLE_TOKEN
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+    let reply = match action {
+        FTAction::AddAdmin { admin_id } => ft.add_admin(&admin_id),
+        FTAction::DeleteAdmin { admin_id } => ft.delete_admin(&admin_id),
+        FTAction::Transfer {
+            tx_id,
+            from,
+            to,
+            amount,
+        } => ft.transfer(tx_id, &from, &to, amount),
+        FTAction::Approve { tx_id, to, amount } => ft.approve(tx_id, &to, amount),
         FTAction::BalanceOf(account) => {
             let balance = ft.balances.get(&account).unwrap_or(&0);
-            msg::reply(FTEvent::Balance(*balance), 0).unwrap();
+            Ok(FTReply::Balance(*balance))
         }
-    }
+    };
+    msg::reply(reply, 0).expect("Error in sending a reply");
 }
 
 #[no_mangle]
 extern fn init() {
-    let config: InitConfig = msg::load().expect("Unable to decode InitConfig");
+    let init_config: InitConfig = msg::load().expect("Unable to decode InitConfig");
     let ft = FungibleToken {
-        name: config.name,
-        symbol: config.symbol,
-        decimals: config.decimals,
+        name: init_config.name,
+        symbol: init_config.symbol,
+        decimals: init_config.decimals,
+        config: init_config.config,
         ..Default::default()
     };
     unsafe { FUNGIBLE_TOKEN = Some(ft) };
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub enum State {
-    Name,
-    Symbol,
-    Decimals,
-    TotalSupply,
-    BalanceOf(ActorId),
-}
-
-#[derive(Debug, Encode, Decode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub enum StateReply {
-    Name(String),
-    Symbol(String),
-    Decimals(u8),
-    TotalSupply(u128),
-    Balance(u128),
+#[no_mangle]
+extern fn state() {
+    let token = unsafe {
+        FUNGIBLE_TOKEN
+            .take()
+            .expect("Unexpected: Error in getting contract state")
+    };
+    let query: Query = msg::load().expect("Unable to decode the query");
+    let reply = match query {
+        Query::Name => {
+            QueryReply::Name(token.name)
+        }
+        Query::Symbol => {
+            QueryReply::Symbol(token.symbol)
+        }
+        Query::Decimals => {
+            QueryReply::Decimals(token.decimals)
+        }
+        Query::BalanceOf(account) => {
+            let balance = if let Some(balance) = token.balances.get(&account) {
+                *balance
+            } else {
+                0
+            };
+            QueryReply::Balance(balance)
+        }
+        Query::AllowanceOfAccount {
+            account,
+            approved_account,
+        } => {
+            let allowance = if let Some(allowance) = token
+                .allowances
+                .get(&account)
+                .and_then(|m| m.get(&approved_account))
+            {
+                *allowance
+            } else {
+                0
+            };
+            QueryReply::AllowanceOfAccount(allowance)
+        }
+        Query::Admins => {
+            QueryReply::Admins(token.admins)
+        }
+        Query::GetTxValidityTime { account, tx_id } => {
+            let valid_until = token.tx_ids.get(&(account, tx_id)).unwrap_or(&0);
+            QueryReply::TxValidityTime(*valid_until)
+        }
+        Query::GetTxIdsForAccount { account } => {
+            let tx_ids: Vec<TxId> = if let Some(tx_ids) =  token.account_to_tx_ids.get(&account).cloned() {
+                tx_ids.into_iter().collect()
+            } else {
+                Vec::new()
+            };
+            QueryReply::TxIdsForAccount { tx_ids }
+        }
+    };
+    msg::reply(reply, 0).expect("Error on sharinf state");
 }
