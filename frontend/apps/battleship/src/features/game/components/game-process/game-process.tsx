95b9ca51220806f2954eeade83e69aafbb1c225d
Yauheni <avegen.dr@gmail.com>
2024-04-06 00:05:09 +0300

    BattleShip: Fixes cells allowed interacting when the game is finished

--- frontend/apps/battleship/src/features/game/components/game-process/game-process.tsx
+++ frontend/apps/battleship/src/features/game/components/game-process/game-process.tsx
@@ -76,23 +76,26 @@ export default function GameProcess() {
     if (!gasless.isLoading) {
       setDisabledCell(true);
 
-      checkBalance(gasLimit, () =>
-        message({
-          payload: { Turn: { step: indexCell } },
-          onInBlock: (messageId) => {
-            if (messageId) {
+      checkBalance(
+        gasLimit,
+        () =>
+          message({
+            payload: { Turn: { step: indexCell } },
+            onInBlock: (messageId) => {
+              if (messageId) {
+                setDisabledCell(false);
+              }
+            },
+            gasLimit,
+            voucherId: gasless.voucherId,
+            onSuccess: () => {
+              setPending(false);
+            },
+            onError: () => {
               setDisabledCell(false);
-            }
-          },
-          gasLimit,
-          voucherId: gasless.voucherId,
-          onSuccess: () => {
-            setPending(false);
-          },
-          onError: () => {
-            setDisabledCell(false);
-          },
-        }),
+            },
+          }),
+        () => setDisabledCell(false),
       );
     }
   };
@@ -164,7 +167,7 @@ export default function GameProcess() {
           sizeBlock={86}
           onClickCell={onClickCell}
           shipStatusArray={enemiesShips}
-          isDisabledCell={isDisabledCell || gasless.isLoading}
+          isDisabledCell={isDisabledCell || gasless.isLoading || gameState.gameOver}
         />
       </div>
 
