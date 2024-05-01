37bec448036265c5d07cafdfb67331e0a0bce553
Vadim Tokar <tokar@dev.vtb.ru>
2024-05-01 09:51:54 +0400

    Tic-tac-toe: add ez-transactions to menu

--- frontend/apps/tic-tac-toe/src/components/layout/header/header.tsx
+++ frontend/apps/tic-tac-toe/src/components/layout/header/header.tsx
@@ -1,3 +1,4 @@
+import { EzGaslessTransactions, EzSignlessTransactions } from '@dapps-frontend/ez-transactions';
 import { Logo } from './logo';
 import styles from './header.module.scss';
 import { Header as CommonHeader, MenuHandler } from '@dapps-frontend/ui';
@@ -13,7 +14,14 @@ export function Header() {
         <Logo className={clsx(styles.header__logo, !account && styles['header__logo--center'])} label="Tic-Tac-Toe" />
       }
       className={{ header: styles.header, content: styles.header__container }}
-      menu={<MenuHandler />}
+      menu={
+        <MenuHandler
+          customItems={[
+            { key: 'signless', option: <EzSignlessTransactions /> },
+            { key: 'gasless', option: <EzGaslessTransactions /> },
+          ]}
+        />
+      }
     />
   );
 }
--- frontend/packages/ui/src/components/menu-handler/menu-handler.tsx
+++ frontend/packages/ui/src/components/menu-handler/menu-handler.tsx
@@ -8,7 +8,7 @@ import styles from './menu-handler.module.css';
 import { MenuOptions, MobileMenu } from '..';
 import { MenuOptionsClassNameProps } from '../menu-options';
 import { MobileMenuClassNameProps } from '../mobile-menu';
-import { useClickOutside } from '@/utils';
+import { useClickOutside, useRootModalRef } from '@/utils';
 import clsx from 'clsx';
 import { useAccount } from '@gear-js/react-hooks';
 import { WalletClassNameProps } from '@/features/wallet-new/components/wallet';
@@ -39,9 +39,15 @@ export function MenuHandler({ customItems, className }: Props) {
   const openMenu = () => setIsMenuOpen(true);
   const closeMenu = () => setIsMenuOpen(false);
 
-  useClickOutside(() => {
-    closeMenu();
-  }, menuRef);
+  const modalRootRef = useRootModalRef();
+
+  useClickOutside(
+    () => {
+      closeMenu();
+    },
+    menuRef,
+    modalRootRef,
+  );
 
   return (
     <div className={clsx(styles.container, className?.container)} ref={menuRef}>
