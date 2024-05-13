f7c73ca5ab1f75cd6980a3c21fde76a761cc3c01
yuriy <yuriy.faizov@gmail.com>
2024-05-13 14:48:35 +0300

    Varaman: add gasless and signless

---
+++ frontend/apps/vara-man/src/hooks/use-on-screen.tsx
@@ -0,0 +1,22 @@
+import { RefObject, useEffect, useMemo, useState } from 'react'
+
+export default function useOnScreen(ref: RefObject<HTMLElement>) {
+	const [isIntersecting, setIntersecting] = useState(false)
+
+	const observer = useMemo(
+		() =>
+			new IntersectionObserver(([entry]) =>
+				setIntersecting(entry.isIntersecting)
+			),
+		[ref]
+	)
+
+	useEffect(() => {
+		if (ref.current) {
+			observer.observe(ref.current)
+			return () => observer.disconnect()
+		}
+	}, [])
+
+	return isIntersecting
+}
