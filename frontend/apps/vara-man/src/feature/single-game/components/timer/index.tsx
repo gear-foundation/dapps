6aa73d04b866afa060d668502b22db896014bdaa
yuriy <yuriy.faizov@gmail.com>
2024-05-30 14:55:20 +0300

    Varaman: fix timer restart game

--- frontend/apps/vara-man/src/feature/single-game/components/timer/index.tsx
+++ frontend/apps/vara-man/src/feature/single-game/components/timer/index.tsx
@@ -1,41 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
-
 import { GAME_OVER } from '@/feature/game/consts';
 
-export const GameTimer = () => {
-	const [, setGameOver] = useAtom(GAME_OVER);
+type Props = {
+  gameRestarted: boolean;
+};
 
-	const [timeLeft, setTimeLeft] = useState('');
+export const GameTimer = ({ gameRestarted }: Props) => {
+  const [, setGameOver] = useAtom(GAME_OVER);
+  const [timeLeft, setTimeLeft] = useState('');
+  const [startTime, setStartTime] = useState<number | null>(null);
 
-	const [startTime, setStartTime] = useState<number | null>(null);
+  useEffect(() => {
+    const start = Date.now();
+    setStartTime(start);
 
-	useEffect(() => {
-		const start = Date.now();
-		setStartTime(start);
+    const totalGameTime = 10 * 60 * 1000;
 
-		const totalGameTime = 10 * 60 * 1000;
+    const updateTimer = () => {
+      const now = Date.now();
+      const elapsedTime = now - (startTime || now);
+      const remainingTime = totalGameTime - elapsedTime;
 
-		const updateTimer = () => {
-			const now = Date.now();
-			const elapsedTime = now - (startTime || now);
-			const remainingTime = totalGameTime - elapsedTime;
+      if (remainingTime <= 0) {
+        setTimeLeft('00:00');
+        setGameOver(true);
+      } else {
+        const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
+        const seconds = Math.floor((remainingTime / 1000) % 60);
+        setTimeLeft(`${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+      }
+    };
 
-			if (remainingTime <= 0) {
-				setTimeLeft(`00:00`);
-				setGameOver(true);
-			} else {
-				const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
-				const seconds = Math.floor((remainingTime / 1000) % 60);
-				setTimeLeft(`${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
-			}
-		};
-		const timerId = setInterval(updateTimer, 1000);
+    const timerId = setInterval(updateTimer, 1000);
 
-		return () => clearInterval(timerId);
-	}, [setGameOver, startTime]);
+    return () => clearInterval(timerId);
+  }, [setGameOver, startTime, gameRestarted]);
 
-	return (
-		<>{timeLeft}</>
-	);
-}
+  return <>{timeLeft}</>;
+};
