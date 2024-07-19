60ce07d33247d230b6b01271746f2c86b48541f9
Magorsky <d.devops.i.200@gmail.com>
2024-07-19 17:29:47 +0300

    fix

--- scripts/update_cargo_toml.py
+++ scripts/update_cargo_toml.py
@@ -5,14 +5,18 @@ from packaging import version
 # GitHub API URL to get the latest tags
 GEAR_REPO_TAGS_URL = "https://api.github.com/repos/gear-tech/gear/tags"
 
-def get_latest_tag():
+# ABSOLUTE PATH to cargo file
+CARGO_FILE_PATH = '../contracts/Cargo.toml'
+
+def get_latest_semver_tag():
     response = requests.get(GEAR_REPO_TAGS_URL)
     response.raise_for_status()
     tags = response.json()
-    if tags:
-        latest_tag = tags[0]['name']
-        return latest_tag
-    return None
+    # Filter out tags that are not valid semantic versions
+    valid_tags = [tag['name'] for tag in tags if re.match(r'^v?\d+\.\d+\.\d+$', tag['name'])]
+    # Sort the valid tags by version
+    valid_tags.sort(key=lambda s: version.parse(s.lstrip('v')), reverse=True)
+    return valid_tags[0] if valid_tags else None
 
 def update_cargo_toml(file_path, new_version):
     with open(file_path, 'r') as file:
@@ -22,13 +26,13 @@ def update_cargo_toml(file_path, new_version):
     content = re.sub(r'gear-wasm-builder = ".*?"', f'gear-wasm-builder = "{new_version}"', content)
     content = re.sub(r'gmeta = ".*?"', f'gmeta = "{new_version}"', content)
     content = re.sub(r'gclient = ".*?"', f'gclient = "{new_version}"', content)
-    content = re.sub(r'gtest = { git = "https://github.com/gear-tech/gear", tag = ".*?" }', f'gtest = {{ git = "https://github.com/gear-tech/gear", tag = "{new_version}" }}', content)
+    content = re.sub(r'gtest = { git = "https://github.com/gear-tech/gear", tag = ".*?" }', f'gtest = {{ git = "https://github.com/gear-tech/gear", tag = "v{new_version}" }}', content)
     content = re.sub(r'gear-core = ".*?"', f'gear-core = "{new_version}"', content)
 
     with open(file_path, 'w') as file:
         file.write(content)
 
 if __name__ == "__main__":
-    new_version = get_latest_tag().lstrip('v')
+    new_version = get_latest_semver_tag().lstrip('v')
     if new_version:
         update_cargo_toml('../contracts/Cargo.toml', new_version)
