e097253ae4c3a68523bc171ea16da15896c53334
Vadim Tokar <vtokar@gear.foundation>
2024-09-03 18:51:26 +0400

    fix: remove game cancelled modal in game

--- frontend/apps/battleship-zk/src/features/multiplayer/components/multiplayer/multiplayer.tsx
+++ frontend/apps/battleship-zk/src/features/multiplayer/components/multiplayer/multiplayer.tsx
@@ -91,23 +91,18 @@ export function Multiplayer() {
       {isGameLeft && <GameCancelledModal text={'Your opponent has left the game.'} onClose={onGameLeft} />}
     </>
   ) : (
-    <>
-      <GameProcess
-        gameType={gameType}
-        totalShoots={totalShoots}
-        successfulShoots={successfulShoots}
-        gameResults={gameEndResult ? { totalTime: gameEndResult.total_time, winner: gameEndResult.winner } : null}
-        remainingTime={remainingTime}
-        gameUpdatedEvent={gameUpdatedEvent}
-        admin={game?.admin}
-        onClickCell={handleClickCell}
-        onExitGame={exitGame}
-        onVerifyOponentsHit={verifyOponentsHit}
-        resetGameState={resetGameState}
-      />
-      {isGameCancelled && (
-        <GameCancelledModal text={'The game was terminated by the administrator.'} onClose={onGameCancelled} />
-      )}
-    </>
+    <GameProcess
+      gameType={gameType}
+      totalShoots={totalShoots}
+      successfulShoots={successfulShoots}
+      gameResults={gameEndResult ? { totalTime: gameEndResult.total_time, winner: gameEndResult.winner } : null}
+      remainingTime={remainingTime}
+      gameUpdatedEvent={gameUpdatedEvent}
+      admin={game?.admin}
+      onClickCell={handleClickCell}
+      onExitGame={exitGame}
+      onVerifyOponentsHit={verifyOponentsHit}
+      resetGameState={resetGameState}
+    />
   );
 }
