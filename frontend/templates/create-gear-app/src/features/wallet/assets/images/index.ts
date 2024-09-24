56a42d4369f59fb4910bfd6b49c80ec7400b32ac
Nikita Yutanov <nikitayutanov@gmail.com>
2024-09-24 19:42:40 +0300

    init cga

---
+++ frontend/templates/create-gear-app/src/features/wallet/assets/images/index.ts
@@ -0,0 +1,9 @@
+import CopySVG from './copy.svg?react';
+import EnkryptSVG from './enkrypt.svg?react';
+import ExitSVG from './exit.svg?react';
+import PolkadotSVG from './polkadot.svg?react';
+import SubWalletSVG from './subwallet.svg?react';
+import TalismanSVG from './talisman.svg?react';
+import WalletSVG from './wallet.svg?react';
+
+export { PolkadotSVG, SubWalletSVG, TalismanSVG, EnkryptSVG, WalletSVG, ExitSVG, CopySVG };
