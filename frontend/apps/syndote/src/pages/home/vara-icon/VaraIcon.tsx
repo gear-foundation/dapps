9cdc53cb4fb2059ff723215ed7cded7d70cfd876
Nikita Yutanov <nikitayutanov@gmail.com>
2024-10-03 17:32:20 +0300

    fix hooks bump

--- frontend/apps/syndote/src/pages/home/vara-icon/VaraIcon.tsx
+++ frontend/apps/syndote/src/pages/home/vara-icon/VaraIcon.tsx
@@ -1,15 +1,11 @@
 import { ReactComponent as VaraSVG } from 'assets/images/icons/vara-coin.svg';
 import { ReactComponent as TVaraSVG } from 'assets/images/icons/tvara-coin.svg';
-import { useAccountDeriveBalancesAll, useApi, useBalanceFormat } from '@gear-js/react-hooks';
+import { useApi } from '@gear-js/react-hooks';
 
 function VaraIcon() {
-  const { isApiReady } = useApi();
-  const { getFormattedBalance, getFormattedGasValue } = useBalanceFormat();
-  const balances = useAccountDeriveBalancesAll();
-  const balance =
-    isApiReady && balances?.freeBalance ? getFormattedBalance(balances.freeBalance.toString()) : undefined;
+  const { api } = useApi();
 
-  return balance?.unit?.toLowerCase() === 'vara' ? <VaraSVG /> : <TVaraSVG />;
+  return api?.registry.chainTokens[0].toLowerCase() === 'vara' ? <VaraSVG /> : <TVaraSVG />;
 }
 
 export { VaraIcon };
