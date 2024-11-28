95b3aa77544fae61200f1cf39d44616d93db3c58
LouiseMedova <luisa.medova@gmail.com>
2024-11-28 14:42:10 +0700

    counter tests

--- contracts/proxy-example/counter/tests/gtest.rs
+++ contracts/proxy-example/counter/tests/gtest.rs
@@ -8,7 +8,7 @@ use counter_client::traits::*;
 const ACTOR_ID: u64 = 42;
 
 #[tokio::test]
-async fn do_something_works() {
+async fn increment_works() {
     let system = System::new();
     system.init_logger();
     system.mint_to(ACTOR_ID, 100_000_000_000_000);
@@ -16,13 +16,12 @@ async fn do_something_works() {
     let remoting = GTestRemoting::new(system, ACTOR_ID.into());
     remoting.system().init_logger();
 
-    // Submit program code into the system
     let program_code_id = remoting.system().submit_code(counter::WASM_BINARY);
 
     let program_factory = counter_client::CounterFactory::new(remoting.clone());
 
     let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
+        .new()
         .send_recv(program_code_id, b"salt")
         .await
         .unwrap();
@@ -30,16 +29,20 @@ async fn do_something_works() {
     let mut service_client = counter_client::Counter::new(remoting.clone());
 
     let result = service_client
-        .do_something() // Call service's method (see app/src/lib.rs:14)
+        .increment()
         .send_recv(program_id)
         .await
         .unwrap();
 
-    assert_eq!(result, "Hello from Counter!".to_string());
+    assert_eq!(result, 1);
+
+    let result = service_client.get_value().recv(program_id).await.unwrap();
+
+    assert_eq!(result, 1);
 }
 
 #[tokio::test]
-async fn get_something_works() {
+async fn decrement_works() {
     let system = System::new();
     system.init_logger();
     system.mint_to(ACTOR_ID, 100_000_000_000_000);
@@ -47,24 +50,47 @@ async fn get_something_works() {
     let remoting = GTestRemoting::new(system, ACTOR_ID.into());
     remoting.system().init_logger();
 
-    // Submit program code into the system
     let program_code_id = remoting.system().submit_code(counter::WASM_BINARY);
 
     let program_factory = counter_client::CounterFactory::new(remoting.clone());
 
     let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
+        .new()
         .send_recv(program_code_id, b"salt")
         .await
         .unwrap();
 
-    let service_client = counter_client::Counter::new(remoting.clone());
+    let mut service_client = counter_client::Counter::new(remoting.clone());
 
     let result = service_client
-        .get_something() // Call service's query (see app/src/lib.rs:19)
-        .recv(program_id)
+        .increment()
+        .send_recv(program_id)
         .await
         .unwrap();
 
-    assert_eq!(result, "Hello from Counter!".to_string());
+    assert_eq!(result, 1);
+
+    let result = service_client
+        .increment()
+        .send_recv(program_id)
+        .await
+        .unwrap();
+
+    assert_eq!(result, 2);
+
+    let result = service_client.get_value().recv(program_id).await.unwrap();
+
+    assert_eq!(result, 2);
+
+    let result = service_client
+        .decrement()
+        .send_recv(program_id)
+        .await
+        .unwrap();
+
+    assert_eq!(result, 1);
+
+    let result = service_client.get_value().recv(program_id).await.unwrap();
+
+    assert_eq!(result, 1);
 }
