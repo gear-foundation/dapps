a9229fb78c6b79fe16a500ec8d50d092d0d723c1
Vadim Tokar <52737608+vraja-nayaka@users.noreply.github.com>
2024-12-09 11:51:35 +0400

    Nft Marketplace: add sails (#518)

--- frontend/apps/nft-marketplace/src/utils/form.ts
+++ frontend/apps/nft-marketplace/src/utils/form.ts
@@ -1,21 +1,10 @@
-import { CID } from 'ipfs-http-client';
-
 const getMintDetails = (attributesValue?: { key: string; value: string }[], rarity?: string) => {
   const attributes = attributesValue?.reduce((accumulator, { key, value }) => ({ ...accumulator, [key]: value }), {});
 
-  return JSON.stringify({ attributes, rarity });
+  const jsonContent = JSON.stringify({ attributes, rarity });
+
+  const file = new Blob([jsonContent], { type: 'application/json' });
+  return file;
 };
 
-const getMintPayload = (name: string, description: string, imgCid: CID, detailsCid?: CID) => {
-  const tokenMetadata = {
-    name,
-    description,
-    media: imgCid.toString(),
-    reference: detailsCid ? detailsCid.toString() : '',
-  };
-
-  // TODO: drop transaction id
-  return { Mint: { tokenMetadata, transaction_id: Math.floor(Math.random() * 1000) } };
-};
-
-export { getMintDetails, getMintPayload };
+export { getMintDetails };
