e72eb389a68f783a5bde078530129e22a4bc8602
Vadim Tokar <52737608+vraja-nayaka@users.noreply.github.com>
2024-12-12 11:24:18 +0400

    W3bstreaming: add sails (#543)

--- frontend/apps/w3bstreaming/src/hooks.ts
+++ frontend/apps/w3bstreaming/src/hooks.ts
@@ -1,46 +1,9 @@
 import { useEffect, useState, MutableRefObject, RefObject, useCallback } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { stringShorten } from '@polkadot/util';
-import { AnyJson, AnyNumber } from '@polkadot/types/types';
-import { HexString, ProgramMetadata } from '@gear-js/api';
-import {
-  useAlert,
-  useHandleCalculateGas as useCalculateGasNative,
-  withoutCommas,
-  useApi,
-  useAccount,
-  useBalanceFormat,
-} from '@gear-js/react-hooks';
-import { useAtom } from 'jotai';
-import { useDnsProgramIds } from '@dapps-frontend/hooks';
-import { LOCAL_STORAGE, SEARCH_PARAMS } from '@/consts';
-import { Handler, StreamsState, UsersState } from '@/types';
-import {
-  IS_STREAMS_READ_ATOM,
-  IS_USERS_READ_ATOM,
-  STREAM_TEASERS_ATOM,
-  USERS_ATOM,
-} from '@/atoms';
-import { useAccountAvailableBalance } from './features/Wallet/hooks';
-import { useGetStreamMetadata } from './features/CreateStream/hooks';
 
-
-function useContractAddressSetup() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { programId } = useDnsProgramIds();
-  // const address = useContractAddress();
-
-  useEffect(() => {
-    if (!programId) return;
-
-    localStorage.setItem(LOCAL_STORAGE.CONTRACT_ADDRESS, programId);
-
-    searchParams.set(SEARCH_PARAMS.MASTER_CONTRACT_ID, programId);
-    setSearchParams(searchParams);
-  }, [programId, searchParams, setSearchParams]);
-}
-
-function useClickOutside(handler: Handler, ...refs: (RefObject<HTMLElement> | MutableRefObject<HTMLElement>)[]): void {
+function useClickOutside(
+  handler: (event: Event) => void,
+  ...refs: (RefObject<HTMLElement> | MutableRefObject<HTMLElement>)[]
+): void {
   useEffect(() => {
     const listener = (event: Event): void => {
       const existingRefs = refs.filter((item) => item?.current && item);
@@ -58,24 +21,6 @@ function useClickOutside(handler: Handler, ...refs: (RefObject<HTMLElement> | Mu
       document.removeEventListener('mousedown', listener);
     };
   }, [refs, handler]);
-}
-
-function useProgramMetadata(source: string) {
-  const alert = useAlert();
-
-  const [metadata, setMetadata] = useState<ProgramMetadata>();
-
-  useEffect(() => {
-    fetch(source)
-      .then((response) => response.text())
-      .then((raw) => ProgramMetadata.from(`0x${raw}`))
-      .then((result) => setMetadata(result))
-      .catch(({ message }: Error) => alert.error(message));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return metadata;
 }
 
 function useMediaQuery(width: number) {
@@ -105,135 +50,4 @@ function useMediaQuery(width: number) {
   return targetReached;
 }
 
-function useProgramState() {
-  const { meta } = useGetStreamMetadata();
-  const { api } = useApi();
-  const { programId } = useDnsProgramIds();
-  const [streamTeasers, setStreamTeasers] = useAtom(STREAM_TEASERS_ATOM);
-  const [users, setUsers] = useAtom(USERS_ATOM);
-  const [isStreamsRead, setIsStreamsRead] = useAtom(IS_STREAMS_READ_ATOM);
-  const [isUsersRead, setIsUsersRead] = useAtom(IS_USERS_READ_ATOM);
-
-  const triggerStreamsState = useCallback(() => {
-    if (!api || !meta || !programId) return;
-
-    const payload = {
-      Streams: null,
-    };
-
-    api.programState
-      .read({ programId, payload }, meta)
-      .then((codec) => codec.toHuman())
-      .then((state: any) => {
-        setStreamTeasers(
-          (state as StreamsState).Streams.reduce(
-            (acc, item) => ({
-              ...acc,
-              [item[0]]: item[1],
-            }),
-            {},
-          ),
-        );
-        setIsStreamsRead(true);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [api, meta, programId, setStreamTeasers]);
-
-  const triggerUsersState = useCallback(() => {
-    if (!api || !meta || !programId) return;
-
-    const payload = {
-      Users: null,
-    };
-
-    api.programState
-      .read({ programId, payload }, meta)
-      .then((codec) => codec.toHuman())
-      .then((state: any) => {
-        setUsers(
-          (state as UsersState).Users.reduce(
-            (acc, item) => ({
-              ...acc,
-              [item[0]]: item[1],
-            }),
-            {},
-          ),
-        );
-        setIsUsersRead(true);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [api, meta, programId, setUsers]);
-
-  useEffect(() => {
-    if (!isStreamsRead) {
-      triggerStreamsState();
-    }
-  }, [isStreamsRead, triggerStreamsState]);
-
-  useEffect(() => {
-    if (!isUsersRead) {
-      triggerUsersState();
-    }
-  }, [isUsersRead, triggerUsersState]);
-
-  const state = {
-    state: { streamTeasers, users },
-    isStateRead: isStreamsRead && isUsersRead,
-    updateStreams: triggerStreamsState,
-    updateUsers: triggerUsersState,
-  };
-
-  return state;
-}
-
-export function useCheckBalance() {
-  const { api } = useApi();
-  const { account } = useAccount();
-  const { availableBalance } = useAccountAvailableBalance();
-  const { getChainBalanceValue } = useBalanceFormat();
-  const alert = useAlert();
-
-  const checkBalance = (limit: number, callback: () => void, onError?: () => void) => {
-    const chainBalance = Number(getChainBalanceValue(Number(withoutCommas(availableBalance?.value || ''))).toFixed());
-    const valuePerGas = Number(withoutCommas(api!.valuePerGas!.toHuman()));
-    const chainEDeposit = Number(
-      getChainBalanceValue(Number(withoutCommas(availableBalance?.existentialDeposit || ''))).toFixed(),
-    );
-
-    const chainEDepositWithLimit = chainEDeposit + limit * valuePerGas;
-
-    if (!chainBalance || chainBalance < chainEDepositWithLimit) {
-      alert.error(`Low balance on ${stringShorten(account?.decodedAddress || '', 8)}`);
-
-      if (onError) {
-        onError();
-      }
-
-      return;
-    }
-
-    callback();
-  };
-
-  return { checkBalance };
-}
-
-export const useHandleCalculateGas = (address: HexString, meta?: ProgramMetadata) => {
-  const { availableBalance } = useAccountAvailableBalance();
-  const calculateGasNative = useCalculateGasNative(address, meta);
-
-  const alert = useAlert();
-
-  return (initPayload: AnyJson, value?: AnyNumber | undefined) => {
-    const balance = Number(withoutCommas(availableBalance?.value || ''));
-    const existentialDeposit = Number(withoutCommas(availableBalance?.existentialDeposit || ''));
-
-    if (!balance || balance < existentialDeposit) {
-      alert.error(`Low balance when calculating gas`);
-    }
-
-    return calculateGasNative(initPayload, value);
-  };
-};
-
-export { useProgramMetadata, useContractAddressSetup, useClickOutside, useMediaQuery, useProgramState };
+export { useClickOutside, useMediaQuery };
