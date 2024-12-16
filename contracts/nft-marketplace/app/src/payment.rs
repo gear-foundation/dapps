16d82314f20d1dd6beae196b1f48251bcbf13f62
MedovTimur <62596970+MedovTimur@users.noreply.github.com>
2024-12-16 12:25:15 +0300

    Adding sails marketplace contract (#513)

---
+++ contracts/nft-marketplace/app/src/payment.rs
@@ -0,0 +1,16 @@
+use extended_vft_client::vft::io as vft_io;
+use sails_rs::gstd::msg;
+use sails_rs::prelude::*;
+
+pub async fn transfer_tokens(
+    ft_contract_id: &ActorId,
+    sender: &ActorId,
+    recipient: &ActorId,
+    value: U256,
+) {
+    let request = vft_io::TransferFrom::encode_call(*sender, *recipient, value);
+    msg::send_bytes_with_gas_for_reply(*ft_contract_id, request, 5_000_000_000, 0, 0)
+        .expect("Error in sending message to ft contract: `TransferFrom`")
+        .await
+        .expect("Error in receiving message to ft contract: `TransferFrom`");
+}
