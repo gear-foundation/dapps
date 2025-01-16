56986ad79aac64b135f1693a03200e691372e9af
MedovTimur <62596970+MedovTimur@users.noreply.github.com>
2025-01-16 10:05:47 +0300

    Adding sails version of oracle (#548)

--- contracts/oracle/randomness/io/src/config.rs
+++
@@ -1,8 +0,0 @@
-use gstd::{prelude::*, ActorId, Decode, Encode, TypeInfo};
-
-#[derive(Debug, Encode, Decode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub struct InitConfig {
-    pub manager: ActorId,
-}
