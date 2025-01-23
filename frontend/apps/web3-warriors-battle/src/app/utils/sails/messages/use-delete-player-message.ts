e87a6fe545a5aa2731e4ad2b9ebd36e989924f90
Nikita Yutanov <nikitayutanov@gmail.com>
2025-01-23 20:02:49 +0300

    add ez transaction handlers

--- frontend/apps/web3-warriors-battle/src/app/utils/sails/messages/use-delete-player-message.ts
+++ frontend/apps/web3-warriors-battle/src/app/utils/sails/messages/use-delete-player-message.ts
@@ -1,21 +1,30 @@
+import { HexString } from '@gear-js/api';
 import { usePrepareProgramTransaction } from '@gear-js/react-hooks';
+import { usePrepareEzTransactionParams } from 'gear-ez-transactions';
+
 import { useProgram } from '@/app/utils';
 import { Options, useSignAndSend } from '@/app/hooks/use-sign-and-send';
 
 export const useDeletePlayerMessage = () => {
   const program = useProgram();
+
   const { prepareTransactionAsync } = usePrepareProgramTransaction({
     program,
     serviceName: 'battle',
     functionName: 'deletePlayer',
   });
+
+  const { prepareEzTransactionParams } = usePrepareEzTransactionParams();
   const { signAndSend } = useSignAndSend();
 
-  const deletePlayerMessage = async (playerId: string, options?: Options) => {
+  const deletePlayerMessage = async (playerId: HexString, options?: Options) => {
+    const { sessionForAccount, ...params } = await prepareEzTransactionParams();
+
     const { transaction } = await prepareTransactionAsync({
-      args: [playerId],
-      gasLimit: { increaseGas: 10 },
+      args: [playerId, sessionForAccount],
+      ...params,
     });
+
     signAndSend(transaction, options);
   };
 
