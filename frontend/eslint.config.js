f5f42dd8abd6eec25dfa5cfa7c4a5b3357ac9ca5
Nikita Yutanov <nikitayutanov@gmail.com>
2025-02-06 22:04:16 +0300

    add config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,8 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   { ignores: ['dist', 'build'] },
@@ -12,10 +14,13 @@ export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       react.configs.flat.recommended,
       react.configs.flat['jsx-runtime'],
       jsxA11y.flatConfigs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      eslintPluginPrettierRecommended,
     ],
 
     files: ['**/*.{ts,tsx}'],
@@ -23,6 +28,12 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2021,
       globals: globals.browser,
+
+      // https://typescript-eslint.io/getting-started/typed-linting
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
 
     // TODO: simplify after updates?
@@ -31,9 +42,38 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
 
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': { typescript: true },
+    },
+
     rules: {
+      // plugins
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // import sort
+      'import/order': [
+        1,
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc' },
+          pathGroups: [{ pattern: '@dapps-frontend/**', group: 'internal', position: 'before' }],
+          pathGroupsExcludedImportTypes: ['builtin', 'object'], // override default for internal
+        },
+      ],
+
+      // dx
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-shadow': 'error',
+      'no-shadow-restricted-names': 'error',
+
+      // react-hook-form onSubmit
+      '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+
+      // we're using typescript
+      'react/prop-types': 'off',
     },
   },
 );
