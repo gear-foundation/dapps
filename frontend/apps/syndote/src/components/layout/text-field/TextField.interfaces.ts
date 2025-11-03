07b747207f8d50b13283e533139c82f71b306d35
Vadim Tokar <52737608+vraja-nayaka@users.noreply.github.com>
2025-11-03 12:15:53 +0300

    chore(syndote): resolve eslint warnings (#639)

--- frontend/apps/syndote/src/components/layout/text-field/TextField.interfaces.ts
+++ frontend/apps/syndote/src/components/layout/text-field/TextField.interfaces.ts
@@ -1,7 +1,6 @@
 import { InputHTMLAttributes } from 'react';
 
 export interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
-  size?: 'large' | 'medium' | 'small';
   label?: string;
   variant?: 'default' | 'active';
   icon?: React.ReactNode;
